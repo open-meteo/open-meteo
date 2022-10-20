3c63b0ef6f2a5884cd098a322deefb7f8f0f45db
patrick-zippenfenig <patrick@zippenfenig.de>
2022-10-20 14:52:52 +0200

    Use only 1 thread for NIO, to maybe reduce download issues in gfs/hrrrr

--- Sources/openmeteo-api/main.swift
+++ Sources/openmeteo-api/main.swift
@@ -13,7 +13,11 @@ FileManager.default.changeCurrentDirectoryPath(projectHome)
 
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
-let app = Application(env)
+
+// Use only 1 thread for NIO, to maybe reduce download issues in gfs/hrrrr
+let cores = env.arguments.count >= 2 && env.arguments[1].starts(with: "download") ? 1 : System.coreCount
+
+let app = Application(env, .shared(MultiThreadedEventLoopGroup(numberOfThreads: cores)))
 defer { app.shutdown() }
 try configure(app)
 try app.run()
