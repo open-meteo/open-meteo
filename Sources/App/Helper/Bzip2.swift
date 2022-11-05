6a9eac2857fdb471b658a0a28d2823e30b1f0503
patrick-zippenfenig <patrick@zippenfenig.de>
2022-11-05 17:46:57 +0100

    thread safe bzip2 decompress

--- Sources/App/Helper/Bzip2.swift
+++ Sources/App/Helper/Bzip2.swift
@@ -10,6 +10,55 @@ extension AsyncSequence where Element == ByteBuffer {
     }
 }
 
+/// Gzip2 uncompressor. Thread safe. Streaming
+public final actor Bzip2DecompressActor {
+    private var bz2: bz_stream
+    private var writebuffer: ByteBuffer
+
+    fileprivate init() {
+        self.writebuffer = ByteBuffer()
+        self.bz2 = bz_stream()
+        writebuffer.reserveCapacity(minimumWritableBytes: 4096)
+        
+        let error = BZ2_bzDecompressInit(&bz2, 0, 0)
+        guard error == BZ_OK else {
+            fatalError("BZ2_bzDecompressInit failed \(error)")
+        }
+    }
+
+    public func consume(_ compressed: ByteBuffer) async throws -> ByteBuffer {
+        var compressed = compressed
+        writebuffer.moveReaderIndex(to: writebuffer.writerIndex)
+        bz2.avail_in = UInt32(compressed.readableBytes)
+        
+        return compressed.withUnsafeMutableReadableBytes({ compressed in
+            bz2.next_in = compressed.baseAddress?.assumingMemoryBound(to: CChar.self)
+            while true {
+                writebuffer.reserveCapacity(minimumWritableBytes: writebuffer.writerIndex)
+                let ret = writebuffer.withUnsafeMutableWritableBytes({ out in
+                    bz2.next_out = out.baseAddress?.assumingMemoryBound(to: CChar.self)
+                    bz2.avail_out = UInt32(out.count)
+                    return BZ2_bzDecompress(&bz2)
+                })
+                guard ret == BZ_OK || ret == BZ_STREAM_END else {
+                    fatalError("BZ2_bzDecompress failed \(ret)")
+                }
+                writebuffer.moveWriterIndex(forwardBy: writebuffer.writableBytes - Int(bz2.avail_out))
+                if bz2.avail_out > 0 || ret == BZ_STREAM_END {
+                    return writebuffer
+                }
+            }
+        })
+    }
+    
+    deinit {
+        let error = BZ2_bzDecompressEnd(&self.bz2)
+        guard error == BZ_OK else {
+            fatalError("BZ2_bzDecompressEnd failed \(error)")
+        }
+    }
+}
+
 /**
  Decompress incoming data using BZIP2
  */
@@ -17,55 +66,21 @@ struct Bzip2AsyncDecompress<T: AsyncSequence>: AsyncSequence where T.Element == 
     public typealias Element = AsyncIterator.Element
     
     let sequence: T
-
+    
     public final class AsyncIterator: AsyncIteratorProtocol {
+        private let compressor: Bzip2DecompressActor
         private var iterator: T.AsyncIterator
-        private var bz2: bz_stream
-        private var writebuffer: ByteBuffer
-
-        fileprivate init(iterator: T.AsyncIterator) {
+        
+        public init(iterator: T.AsyncIterator) {
             self.iterator = iterator
-            self.writebuffer = ByteBuffer()
-            self.bz2 = bz_stream()
-            writebuffer.reserveCapacity(minimumWritableBytes: 4096)
-            
-            let error = BZ2_bzDecompressInit(&bz2, 0, 0)
-            guard error == BZ_OK else {
-                fatalError("BZ2_bzDecompressInit failed \(error)")
-            }
+            self.compressor = Bzip2DecompressActor()
         }
-
+        
         public func next() async throws -> ByteBuffer? {
             guard var compressed = try await self.iterator.next() else {
                 return nil
             }
-            writebuffer.moveReaderIndex(to: writebuffer.writerIndex)
-            bz2.avail_in = UInt32(compressed.readableBytes)
-            return compressed.withUnsafeMutableReadableBytes({ compressed in
-                bz2.next_in = compressed.baseAddress?.assumingMemoryBound(to: CChar.self)
-                while true {
-                    writebuffer.reserveCapacity(minimumWritableBytes: writebuffer.writerIndex)
-                    let ret = writebuffer.withUnsafeMutableWritableBytes({ out in
-                        bz2.next_out = out.baseAddress?.assumingMemoryBound(to: CChar.self)
-                        bz2.avail_out = UInt32(out.count)
-                        return BZ2_bzDecompress(&bz2)
-                    })
-                    guard ret == BZ_OK || ret == BZ_STREAM_END else {
-                        fatalError("BZ2_bzDecompress failed \(ret)")
-                    }
-                    writebuffer.moveWriterIndex(forwardBy: writebuffer.writableBytes - Int(bz2.avail_out))
-                    if bz2.avail_out > 0 || ret == BZ_STREAM_END {
-                        return writebuffer
-                    }
-                }
-            })
-        }
-        
-        deinit {
-            let error = BZ2_bzDecompressEnd(&self.bz2)
-            guard error == BZ_OK else {
-                fatalError("BZ2_bzDecompressEnd failed \(error)")
-            }
+            return try await compressor.consume(compressed)
         }
     }
 
