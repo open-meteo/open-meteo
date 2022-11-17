88c2e8e56be8ecadd2c95750008ddd61da57dea8
patrick-zippenfenig <patrick@zippenfenig.de>
2022-11-17 16:27:00 +0100

    wip ensemble download

--- Sources/App/GloFas/GloFasDownloader.swift
+++ Sources/App/GloFas/GloFasDownloader.swift
@@ -3,16 +3,28 @@ import Vapor
 import SwiftPFor2D
 import SwiftEccodes
 
-struct GloFasDownloader: Command {
+struct GloFasDownloader: AsyncCommandFix {
     struct Signature: CommandSignature {
+        @Argument(name: "domain")
+        var domain: String
+        
         @Option(name: "timeinterval", short: "t", help: "Timeinterval to download with format 20220101-20220131")
         var timeinterval: String?
         
         @Option(name: "year", short: "y", help: "Download one year")
         var year: String?
         
+        @Flag(name: "skip-existing")
+        var skipExisting: Bool
+        
         @Option(name: "cdskey", short: "k", help: "CDS API user and key like: 123456:8ec08f...")
         var cdskey: String?
+        
+        @Option(name: "ftpuser", short: "u", help: "Username for the ECMWF CAMS FTP server")
+        var ftpuser: String?
+        
+        @Option(name: "ftppassword", short: "p", help: "Password for the ECMWF CAMS FTP server")
+        var ftppassword: String?
         
         /// Get the specified timerange in the command, or use the last 7 days as range
         func getTimeinterval() -> TimerangeDt {
@@ -35,23 +47,103 @@ struct GloFasDownloader: Command {
         "Download ERA5 from the ECMWF climate data store and convert"
     }
     
-    func run(using context: CommandContext, signature: Signature) throws {
+    func run(using context: CommandContext, signature: Signature) async throws {
         let logger = context.application.logger
-        guard let cdskey = signature.cdskey else {
-            fatalError("cds key is required")
+        guard let domain = GloFasDomain.init(rawValue: signature.domain) else {
+            fatalError("Invalid domain '\(signature.domain)'")
         }
         
-        /// Only download one specified year
-        if let yearStr = signature.year {
-            guard let year = Int(yearStr) else {
-                fatalError("Could not convert year to integer")
+        switch domain {
+        case .consolidated:
+            guard let cdskey = signature.cdskey else {
+                fatalError("cds key is required")
             }
-            try downloadYear(logger: logger, year: year, cdskey: cdskey)
-            return
+            /*for year in 2015...2018 {
+                try downloadYear(logger: logger, year: year, cdskey: cdskey)
+                
+            }
+            exit(0)*/
+            
+            /// Only download one specified year
+            if let yearStr = signature.year {
+                guard let year = Int(yearStr) else {
+                    fatalError("Could not convert year to integer")
+                }
+                try downloadYear(logger: logger, year: year, cdskey: cdskey)
+                return
+            }
+        case .forecastv3:
+            let run = Timestamp.now().with(hour: 0)
+            guard let ftpuser = signature.ftpuser else {
+                fatalError("ftpuser is required")
+            }
+            guard let ftppassword = signature.ftppassword else {
+                fatalError("ftppassword is required")
+            }
+            
+            try await downloadEnsembleForecast(application: context.application, domain: GloFasDomain.forecastv3, run: run, skipFilesIfExisting: signature.skipExisting, user: ftpuser, password: ftppassword)
+        }
+    }
+    
+    func downloadEnsembleForecast(application: Application, domain: GloFasDomain, run: Timestamp, skipFilesIfExisting: Bool, user: String, password: String) async throws {
+        try FileManager.default.createDirectory(atPath: domain.downloadDirectory, withIntermediateDirectories: true)
+        let logger = application.logger
+        
+        try FileManager.default.createDirectory(atPath: domain.downloadDirectory, withIntermediateDirectories: true)
+        try FileManager.default.createDirectory(atPath: domain.omfileDirectory, withIntermediateDirectories: true)
+        
+        let nx = domain.grid.nx
+        let ny = domain.grid.ny
+        
+        let writer = OmFileWriter(dim0: nx, dim1: ny, chunk0: nx, chunk1: ny)
+        let nLocationChunk = nx * ny / 1000
+        var grib2d = GribArray2D(nx: nx, ny: ny)
+        
+        let curl = Curl(logger: logger, readTimeout: 20*60)
+        let dateRun = run.format_YYYYMMdd
+        let remote = "https://\(user):\(password)@aux.ecmwf.int/ecpds/data/file/CEMS_Flood_Glofas/fc_grib/\(dateRun)/dis_\(dateRun)00.grib"
+        let file = "\(domain.downloadDirectory)dis.grib"
+        
+        if skipFilesIfExisting && !FileManager.default.fileExists(atPath: file) {
+            try await curl.download(url: remote, toFile: file, client: application.dedicatedHttpClient)
+        }
+        
+        let grib = try GribFile(file: file)
+        for message in grib.messages {
+            /// Date in ISO timestamp string format `20210101`
+            let date = message.get(attribute: "dataDate")!
+            grib.messages.forEach { message in
+                print(
+                    message.get(attribute: "name")!,
+                    message.get(attribute: "shortName")!,
+                    message.get(attribute: "level")!,
+                    message.get(attribute: "paramId")!
+                )
+                //if message.get(attribute: "name") == "unknown" {
+                    message.iterate(namespace: .ls).forEach({print($0)})
+                    message.iterate(namespace: .parameter).forEach({print($0)})
+                    message.iterate(namespace: .mars).forEach({print($0)})
+                    message.iterate(namespace: .all).forEach({print($0)})
+                //}
+            }
+            
+            exit(0);
+            
+            let member = message.get(attribute: "member")!
+            
+            logger.info("Converting day \(date) Member \(member)")
+            let dailyFile = "\(domain.downloadDirectory)glofas_member\(member)_\(date).om"
+            if FileManager.default.fileExists(atPath: dailyFile) {
+                continue
+            }
+            try grib2d.load(message: message)
+            grib2d.array.flipLatitude()
+            //try grib2d.array.writeNetcdf(filename: "\(downloadDir)glofas_\(date).nc")
+           
+            try OmFileWriter(dim0: ny*nx, dim1: 1, chunk0: nLocationChunk, chunk1: 1).write(file: dailyFile, compressionType: .p4nzdec256logarithmic, scalefactor: 1000, all: grib2d.array.data)
         }
         
     }
-    
     
     func downloadYear(logger: Logger, year: Int, cdskey: String) throws {
         let domain = GloFasDomain.consolidated
@@ -117,7 +209,7 @@ struct GloFasDownloader: Command {
             /// Date in ISO timestamp string format `20210101`
             let date = message.get(attribute: "dataDate")!
             logger.info("Converting day \(date)")
-            let dailyFile = "\(downloadDir)glofas_\(date).fpx"
+            let dailyFile = "\(downloadDir)glofas_\(date).om"
             if FileManager.default.fileExists(atPath: dailyFile) {
                 continue
             }
@@ -134,7 +226,7 @@ struct GloFasDownloader: Command {
         let yearlyFile = "\(domain.omfileArchive!)river_discharge_\(year).om"
         
         let omFiles = try time.map { time -> OmFileReader in
-            let omFile = "\(downloadDir)glofas_\(time.format_YYYYMMdd).fpx"
+            let omFile = "\(downloadDir)glofas_\(time.format_YYYYMMdd).om"
             return try OmFileReader(file: omFile)
         }
         
@@ -172,6 +264,7 @@ struct GloFasDownloader: Command {
 
 enum GloFasDomain: String, GenericDomain {
     case consolidated
+    case forecastv3
     
     var omfileDirectory: String {
         return "\(OpenMeteo.dataDictionary)omfile-glofas-\(rawValue)/"
@@ -187,6 +280,8 @@ enum GloFasDomain: String, GenericDomain {
         switch self {
         case .consolidated:
             return RegularGrid(nx: 7200, ny: 3000, latMin: -60, lonMin: -180, dx: 0.05, dy: 0.05)
+        case .forecastv3:
+            return RegularGrid(nx: 3600, ny: 1500, latMin: -60, lonMin: -180, dx: 0.1, dy: 0.1)
         }
     }
     
@@ -202,6 +297,8 @@ enum GloFasDomain: String, GenericDomain {
         switch self {
         case .consolidated:
             return 100 // 100 days per file
+        case .forecastv3:
+            return 60
         }
     }
 }
