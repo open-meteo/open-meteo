ae9a4423de4d641eb3652f3531b788d7cec77767
patrick-zippenfenig <patrick@zippenfenig.de>
2022-11-28 16:30:14 +0100

    fix lambert projection for cerra

--- Sources/App/Domains/LambertConformalConic.swift
+++ Sources/App/Domains/LambertConformalConic.swift
@@ -5,39 +5,40 @@ struct LambertConformalConicProjection: Projectable {
     let ρ0: Float
     let F: Float
     let n: Float
-    let λ0_rad: Float
-    let ϕ0_rad: Float
+    let λ0: Float
     
     /// Radius of Earth
     var R: Float { 6370.997 }
     
-    /// λ0 reference longitude
-    /// ϕ0  reference latitude
-    /// ϕ1 and ϕ2 standard parallels
-    public init(λ0: Float, ϕ0: Float, ϕ1: Float, ϕ2: Float) {
+    /// λ0 reference longitude in degrees
+    /// ϕ0  reference latitude in degrees
+    /// ϕ1 and ϕ2 standard parallels in degrees
+    public init(λ0 λ0_dec: Float, ϕ0 ϕ0_dec: Float, ϕ1 ϕ1_dec: Float, ϕ2 ϕ2_dec: Float) {
         // https://mathworld.wolfram.com/LambertConformalConicProjection.html
-        λ0_rad = (λ0 + 360).truncatingRemainder(dividingBy: 360).degreesToRadians
-        ϕ0_rad = ϕ0.degreesToRadians
-        let ϕ1_rad = ϕ1.degreesToRadians
-        let ϕ2_rad = ϕ2.degreesToRadians
+        // https://pubs.usgs.gov/pp/1395/report.pdf page 104
+        λ0 = (λ0_dec + 360).truncatingRemainder(dividingBy: 360).degreesToRadians
+        let ϕ0 = ϕ0_dec.degreesToRadians
+        let ϕ1 = ϕ1_dec.degreesToRadians
+        let ϕ2 = ϕ2_dec.degreesToRadians
         if ϕ1 == ϕ2 {
-            n = sin(ϕ1_rad)
+            n = sin(ϕ1)
         } else {
-            n = log(cos(ϕ1_rad) / cos(ϕ2_rad)) / log(tan(.pi/4 + ϕ2_rad/2) / tan(.pi/4 + ϕ1_rad/2))
+            n = log(cos(ϕ1) / cos(ϕ2)) / log(tan(.pi/4 + ϕ2/2) / tan(.pi/4 + ϕ1/2))
         }
         
-        F = (cos(ϕ1_rad) * powf(tan(.pi/4 + ϕ1_rad/2), n))/n
-        ρ0 = F/powf(tan(.pi/4 + ϕ0_rad/2),n)
+        F = (cos(ϕ1) * powf(tan(.pi/4 + ϕ1/2), n))/n
+        ρ0 = F/powf(tan(.pi/4 + ϕ0/2),n)
     }
     
     func forward(latitude: Float, longitude: Float) -> (x: Float, y: Float) {
-        let latitudeRadians = latitude.degreesToRadians
-        let longitudeRadians = (longitude + 360).truncatingRemainder(dividingBy: 360).degreesToRadians
-        let delta = longitudeRadians - λ0_rad
+        let ϕ = latitude.degreesToRadians
+        let λ = (longitude + 360).truncatingRemainder(dividingBy: 360).degreesToRadians
+        // If (λ - λ0) exceeds the range:±: 180°, 360° should be added or subtracted.
+        let θ = n * ((λ - λ0 + .pi).truncatingRemainder(dividingBy: 2 * .pi) - .pi)
         
-        let p = F/powf(tan(.pi/4 + latitudeRadians/2), n)
-        let x = R * p * sin(n*(delta))
-        let y = R * (ρ0 - p * cos(n*(delta)))
+        let p = F/powf(tan(.pi/4 + ϕ/2), n)
+        let x = R * p * sin(θ)
+        let y = R * (ρ0 - p * cos(θ))
         return (x, y)
     }
     
@@ -49,7 +50,7 @@ struct LambertConformalConicProjection: Projectable {
         let ρ = (n>0 ? 1 : -1) * sqrt(powf(x_skaliert,2) + powf(ρ0 - y_skaliert,2))
 
         let ϕ_rad = 2*atan(powf(F/ρ, 1/n)) - .pi/2
-        let λ_rad = λ0_rad + θ/n
+        let λ_rad = λ0 + θ/n
 
         let ϕ = ϕ_rad.radiansToDegrees
         let λ = λ_rad.radiansToDegrees
