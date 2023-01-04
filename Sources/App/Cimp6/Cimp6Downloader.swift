baf88422f2fe2f5da90afa1d53abe421dbe10d66
patrick-zippenfenig <patrick@zippenfenig.de>
2023-01-04 16:42:25 +0100

    definitions

--- Sources/App/Cimp6/Cimp6Downloader.swift
+++ Sources/App/Cimp6/Cimp6Downloader.swift
@@ -1,11 +1,6 @@
-//
-//  File.swift
-//  
-//
-//  Created by Patrick Zippenfenig on 03.01.23.
-//
-
 import Foundation
+import Vapor
+
 
 /**
  https://esgf-data.dkrz.de/search/cmip6-dkrz/
@@ -18,6 +13,7 @@ import Foundation
  6h: 2m temp, humidity, wind, surface temp,
  daily: 2m temp, humidity. wind, precip, longwave,
  monthly: temp, clouds, precip, runoff, wind, soil moist 1 level, humidity, snow,
+ NO daily min/max directly
  
  FGOALS-f3  (CAS China) https://www.wdc-climate.de/ui/cmip6?input=CMIP6.HighResMIP.CAS.FGOALS-f3-H.highresSST-future
  0.25°
@@ -25,6 +21,7 @@ import Foundation
  6h: missing temperature for higher altitude,
  day: missing temperature for land,clc, wind, hum, precip, sw,
  monthly: temp, clc, wind, hum, precip,
+ NO daily min/max directly
  
  HiRAM-SIT-HR (RCEC taiwan) https://www.wdc-climate.de/ui/cmip6?input=CMIP6.HighResMIP.AS-RCEC.HiRAM-SIT-HR
  0.23°
@@ -75,8 +72,242 @@ import Foundation
 
  */
 
-enum Cimp6Domain {
-    case actor
+enum Cmip6Domain: String {
+    case CMCC_CM2_VHR4_daily
+    case FGOALS_f3_H_daily
+    case HiRAM_SIT_HR_daily
+    case MRI_AGCM3_2_S_daily
 }
 
+enum Cmip6Variable: String {
+    case pressure_msl
+    case temperature_2m_min
+    case temperature_2m_max
+    case temperature_2m
+    case cloudcover
+    case precipitation
+    case runoff
+    case snowfall
+    case relative_humidity_min
+    case relative_humidity_max
+    case relative_humidity
+    case windspeed_10m
+    /// Total Soil Moisture Content
+    case soil_moisture_total
+    
+    case surface_temperature
+    
+    /// Moisture in Upper Portion of Soil Column
+    case soil_moisture_upper
+    case shortwave
+    case shortwave_clearsky
+    
+    case specific_humidity
+    
+    
+    enum TimeType {
+        case monthly
+        case yearly
+        case tenYearly
+    }
+    
+    func domainTimeRange(for domain: Cmip6Domain) -> TimeType? {
+        switch domain {
+        case .MRI_AGCM3_2_S_daily:
+            switch self {
+            case .pressure_msl:
+                return .yearly
+            case .temperature_2m_min:
+                return .yearly
+            case .temperature_2m_max:
+                return .yearly
+            case .temperature_2m:
+                return .yearly
+            case .cloudcover:
+                return .yearly
+            case .precipitation:
+                return .yearly
+            case .runoff:
+                return .yearly
+            case .snowfall:
+                return .yearly
+            case .relative_humidity_min:
+                return .yearly
+            case .relative_humidity_max:
+                return .yearly
+            case .relative_humidity:
+                return .yearly
+            case .soil_moisture_total:
+                return .yearly
+            case .surface_temperature:
+                return .yearly
+            case .soil_moisture_upper:
+                return .yearly
+            case .shortwave:
+                return .yearly
+            case .shortwave_clearsky:
+                return .yearly
+            case .specific_humidity:
+                return nil
+            case .windspeed_10m:
+                return .yearly
+            }
+        case .CMCC_CM2_VHR4_daily:
+            switch self {
+            case .relative_humidity:
+                return .monthly
+            case .precipitation:
+                // only precip is in yearly files...
+                return .yearly
+            case .temperature_2m:
+                return .monthly
+            case .windspeed_10m:
+                return .monthly
+            default:
+                return nil
+            }
+        case .FGOALS_f3_H_daily:
+            // no near surface RH, only specific humidity
+            switch self {
+            case .specific_humidity:
+                return .yearly
+            case .cloudcover:
+                return .yearly
+            case .temperature_2m:
+                return .yearly
+            case .pressure_msl:
+                return .yearly
+            case .snowfall:
+                return .yearly
+            case .shortwave:
+                return .yearly
+            case .windspeed_10m:
+                return .yearly
+            case .precipitation:
+                return .yearly
+            default:
+                return nil
+            }
+        case .HiRAM_SIT_HR_daily:
+            // no u/v wind components near surface
+            switch self {
+            case .temperature_2m:
+                return .yearly
+            case .temperature_2m_max:
+                return .yearly
+            case .temperature_2m_min:
+                return .yearly
+            case .cloudcover:
+                return .yearly
+            case .precipitation:
+                return .yearly
+            case .snowfall:
+                return .yearly
+            case .relative_humidity:
+                return .yearly
+            case .shortwave:
+                return .yearly
+            case .windspeed_10m:
+                return .yearly
+            default:
+                return nil
+            }
+        }
+    }
+    
+    /// hourly the same but no min/max. Hourly one file per month. Daily = yearly file
+    var shortname: String {
+        switch self {
+        case .pressure_msl:
+            return "psl"
+        case .temperature_2m_min:
+            return "tasmin"
+        case .temperature_2m_max:
+            return "tasmax"
+        case .temperature_2m:
+            return "tas"
+        case .cloudcover:
+            return "clt"
+        case .precipitation:
+            return "pr"
+        case .relative_humidity_min:
+            return "hursmax"
+        case .relative_humidity_max:
+            return "hursmin"
+        case .relative_humidity:
+            return "hurs"
+        case .shortwave_clearsky:
+            return "rsdscs"
+        case .runoff:
+            return "mrro"
+        case .snowfall:
+            return "prsn" //kg m-2 s-1
+        case .soil_moisture_total:
+            return "mrso" // kg m-2
+        case .soil_moisture_upper: // Moisture in Upper Portion of Soil Column
+            return "mrsos"
+        case .shortwave:
+            return "rsds"
+        case .surface_temperature:
+            return "tslsi"
+        case .specific_humidity:
+            return "huss"
+        case .windspeed_10m:
+            return "sfcWind"
+        }
+    }
+}
 
+struct DownloadCimpCommand: AsyncCommandFix {
+    /// 6k locations require around 200 MB memory for a yearly time-series
+    static var nLocationsPerChunk = 6_000
+    
+    struct Signature: CommandSignature {
+        @Argument(name: "domain")
+        var domain: String
+        
+        @Option(name: "year", short: "y", help: "Download one year")
+        var year: String?
+    }
+    
+    var help: String {
+        "Download CMIP6 data and convert"
+    }
+    
+    func run(using context: CommandContext, signature: Signature) async throws {
+        let logger = context.application.logger
+        
+        guard let domain = Cmip6Domain.init(rawValue: signature.domain) else {
+            fatalError("Invalid domain '\(signature.domain)'")
+        }
+        
+        //let variables: [GenericVariable] = domain == .cerra ? CerraVariable.allCases : Era5Variable.allCases.filter({ $0.availableForDomain(domain: domain) })
+        
+        /// Make sure elevation information is present. Otherwise download it
+        //try await downloadElevation(application: context.application, cdskey: cdskey, domain: domain)
+        
+        /// Only download one specified year
+        if let yearStr = signature.year {
+            if yearStr.contains("-") {
+                let split = yearStr.split(separator: "-")
+                guard split.count == 2 else {
+                    fatalError("year invalid")
+                }
+                for year in Int(split[0])! ... Int(split[1])! {
+                    try await runYear(application: context.application, year: year, domain: domain)
+                }
+            } else {
+                guard let year = Int(yearStr) else {
+                    fatalError("Could not convert year to integer")
+                }
+                try await runYear(application: context.application, year: year, domain: domain)
+            }
+            return
+        }
+        fatalError("OK")
+    }
+    
+    func runYear(application: Application, year: Int, domain: Cmip6Domain) async throws {
+        
+    }
+}
