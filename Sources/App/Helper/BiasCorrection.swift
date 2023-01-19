f8d18493cdaacac4a3c3db0d0a2799b3890bf7a0
patrick-zippenfenig <patrick@zippenfenig.de>
2023-01-19 12:51:42 +0100

    cdf seasonal interpolation

--- Sources/App/Helper/BiasCorrection.swift
+++ Sources/App/Helper/BiasCorrection.swift
@@ -92,16 +92,16 @@ struct BiasCorrection {
         switch type {
         case .absoluteChage:
             return zip(forecastTime, forecast).map { (time, forecast) in
-                let epsilon = interpolate(binsForecast, cdfForecast.get(time: time), x: forecast, extrapolate: false)
-                let qdm1 = interpolate(cdfRefernce.get(time: time), binsRefernce, x: epsilon, extrapolate: false)
-                return qdm1 + forecast - interpolate(cdfControl.get(time: time), binsControl, x: epsilon, extrapolate: false)
+                let epsilon = interpolate(binsForecast, cdfForecast, x: forecast, time: time, extrapolate: false)
+                let qdm1 = interpolate(cdfRefernce, binsRefernce, x: epsilon, time: time, extrapolate: false)
+                return qdm1 + forecast - interpolate(cdfControl, binsControl, x: epsilon, time: time, extrapolate: false)
             }
         case .relativeChange:
             let maxScaleFactor: Float = 10
             return zip(forecastTime, forecast).map { (time, forecast) in
-                let epsilon = interpolate(binsForecast, cdfForecast.get(time: time), x: forecast, extrapolate: false)
-                let qdm1 = interpolate(cdfRefernce.get(time: time), binsRefernce, x: epsilon, extrapolate: false)
-                let scale = forecast / interpolate(cdfControl.get(time: time), binsControl, x: epsilon, extrapolate: false)
+                let epsilon = interpolate(binsForecast, cdfForecast, x: forecast, time: time, extrapolate: false)
+                let qdm1 = interpolate(cdfRefernce, binsRefernce, x: epsilon, time: time, extrapolate: false)
+                let scale = forecast / interpolate(cdfControl, binsControl, x: epsilon, time: time, extrapolate: false)
                 return qdm1 / min(max(scale, maxScaleFactor * -1), maxScaleFactor)
             }
         }
@@ -156,17 +156,139 @@ struct BiasCorrection {
         let dydx = xR - xL == 0 ? 0 : (yR - yL) / (xR - xL);  // gradient
         return yL + dydx * (x - xL);       // linear interpolation
     }
+    
+    /// Find value `x` on first array, then interpolate on the second array to return the value
+    static func interpolate<Cdf: MonthlyBinable>(_ xData: Bins, _ yData: Cdf, x: Float, time: Timestamp, extrapolate: Bool) -> Float {
+        //assert(xData.count == yData.count)
+        let size = xData.count
+
+        var i = 0;  // find left end of interval for interpolation
+        if x >= xData[xData.index(xData.startIndex, offsetBy: size - 2)] {
+            i = size - 2;  // special case: beyond right end
+        } else {
+            while (x > xData[xData.index(xData.startIndex, offsetBy: i + 1)]) { i += 1 }
+        }
+        let xL = xData[xData.index(xData.startIndex, offsetBy: i)]
+        var yL = yData.get(bin: i, time: time)
+        let xR = xData[xData.index(xData.startIndex, offsetBy: i + 1)]
+        var yR = yData.get(bin: i+1, time: time) // points on either side (unless beyond ends)
+
+        if !extrapolate {  // if beyond ends of array and not extrapolating
+            if (x < xL) { yR = yL }
+            if (x > xR) { yL = yR }
+        }
+        let dydx = xR - xL == 0 ? 0 : (yR - yL) / (xR - xL);  // gradient
+        return yL + dydx * (x - xL);       // linear interpolation
+    }
+    
+    /// Find value `x` on first array, then interpolate on the second array to return the value
+    static func interpolate<Cdf: MonthlyBinable>(_ xData: Cdf, _ yData: Bins, x: Float, time: Timestamp, extrapolate: Bool) -> Float {
+        //assert(xData.count == yData.count)
+        let size = xData.nBins
+
+        var i = 0;  // find left end of interval for interpolation
+        if x >= xData.get(bin: size - 2, time: time) {
+            i = size - 2;  // special case: beyond right end
+        } else {
+            while (x > xData.get(bin: i+1, time: time)) { i += 1 }
+        }
+        let xL = xData.get(bin: i, time: time)
+        var yL = yData[yData.index(yData.startIndex, offsetBy: i)]
+        let xR = xData.get(bin: i+1, time: time)
+        var yR = yData[yData.index(yData.startIndex, offsetBy: i + 1)] // points on either side (unless beyond ends)
+
+        if !extrapolate {  // if beyond ends of array and not extrapolating
+            if (x < xL) { yR = yL }
+            if (x > xR) { yL = yR }
+        }
+        let dydx = xR - xL == 0 ? 0 : (yR - yL) / (xR - xL);  // gradient
+        return yL + dydx * (x - xL);       // linear interpolation
+    }
+}
+
+
+/*
+ Before
+ QDM projected rmse: 2.1441827
+ QDM projected me: -0.008521517
+ QDM qctime rmse: 2.1524856
+ QDM qctime me: 0.33143386
+ 
+ QDM projected rmse: 2.1441827
+ QDM projected me: -0.008521517
+ QDM qctime rmse: 2.1524856
+ QDM qctime me: 0.33143386
+ 
+ with interpolation
+ QDM projected rmse: 2.144915
+ QDM projected me: -0.021453733
+ QDM qctime rmse: 2.1539917
+ QDM qctime me: 0.3202337
+ 
+ inverted factors
+ QDM projected rmse: 2.1443715
+ QDM projected me: -0.016594684
+ QDM qctime rmse: 2.153304
+ QDM qctime me: 0.3233069
+ 
+ inverted 4x bin:
+ QDM projected rmse: 2.1475098
+ QDM projected me: 0.027171426
+ QDM qctime rmse: 2.155336
+ QDM qctime me: 0.37284786
+ 
+ normal: 12x bin FIXED offset
+ QDM projected rmse: 2.1433
+ QDM projected me: 0.015121117
+ QDM qctime rmse: 2.1498253
+ QDM qctime me: 0.36447984
+ 
+ inverted: 12x bin FIXED offset
+ QDM projected rmse: 2.14071
+ QDM projected me: 0.002004539
+ QDM qctime rmse: 2.1490934
+ QDM qctime me: 0.3547897
+ 
+ normal...
+ QDM projected rmse: 2.14162
+ QDM projected me: 0.017507693
+ QDM qctime rmse: 2.154178
+ QDM qctime me: 0.36449537
+ 
+ 4 months normal:
+ QDM projected rmse: 2.1469715
+ QDM projected me: -0.00023381515
+ QDM qctime rmse: 2.1521297
+ QDM qctime me: 0.3463871
+ 
+ 4 month inverted:
+ QDM projected rmse: 2.1462765
+ QDM projected me: -0.007172498
+ QDM qctime rmse: 2.1524358
+ QDM qctime me: 0.33949688
+ 
+ QUESTION: only do 4 season split?
+ */
+
+protocol MonthlyBinable {
+    func get(bin: Int, time t: Timestamp) -> Float
+    var nBins: Int { get }
 }
 
 /// Calculate CDF for each month individually
-struct CdfMonthly {
+struct CdfMonthly: MonthlyBinable {
     let cdf: [Float]
+    let bins: Bins
     
-    static var binsPerYear: Int { 12 }
+    static var binsPerYear: Int { 4 }
+    
+    var nBins: Int {
+        cdf.count / Self.binsPerYear
+    }
     
     /// input temperature and time axis
     init(vector: ArraySlice<Float>, time: TimerangeDt, bins: Bins) {
-        let count = bins.count
+        let count = bins.nQuantiles
         var cdf = [Float](repeating: 0, count: count * Self.binsPerYear)
         for (t, value) in zip(time, vector) {
             let fractionalDayOfYear = ((t.timeIntervalSince1970 % 31_557_600) + 31_557_600) % 31_557_600
@@ -174,8 +296,8 @@ struct CdfMonthly {
             let fraction = Float(fractionalDayOfYear).truncatingRemainder(dividingBy: Float(31_557_600 / Self.binsPerYear)) / Float(31_557_600 / Self.binsPerYear)
             for (i, bin) in bins.enumerated().reversed() {
                 if value < bin {
-                    cdf[monthBin * count + i] += fraction
-                    cdf[((monthBin+1) % Self.binsPerYear) * count + i] += 1-fraction
+                    cdf[monthBin * count + i] += 1-fraction
+                    cdf[((monthBin+1) % Self.binsPerYear) * count + i] += fraction
                     //cdf[monthBin * count + i] += 1
                     //cdf[((monthBin+1) % binsPerYear) * count + i] += 1
                     //cdf[((monthBin-1+binsPerYear) % binsPerYear) * count + i] += 1
@@ -193,6 +315,7 @@ struct CdfMonthly {
             }
         }
         self.cdf = cdf
+        self.bins = bins
     }
     
     /// month starting at 0
@@ -203,15 +326,27 @@ struct CdfMonthly {
         let binLength = cdf.count / Self.binsPerYear
         return cdf[binLength * monthBin ..< binLength * (monthBin+1)]
     }
+    
+    /// linear interpolate between 2 months CDF
+    func get(bin: Int, time t: Timestamp) -> Float {
+        let fractionalDayOfYear = ((t.timeIntervalSince1970 % 31_557_600) + 31_557_600) % 31_557_600
+        let monthBin = fractionalDayOfYear / (31_557_600 / Self.binsPerYear)
+        let fraction = Float(fractionalDayOfYear).truncatingRemainder(dividingBy: Float(31_557_600 / Self.binsPerYear)) / Float(31_557_600 / Self.binsPerYear)
+        
+        let binLength = cdf.count / Self.binsPerYear
+        return cdf[binLength * monthBin + bin] * (1-fraction) + cdf[binLength * ((monthBin+1) % Self.binsPerYear) + bin] * (fraction)
+    }
 }
 
+
 /// Calculate CDF for each month individually, but over a sliding window of 10 years
-struct CdfMonthly10YearSliding {
+struct CdfMonthly10YearSliding: MonthlyBinable {
     let cdf: [Float]
-    let binLength: Int
+    var nBins: Int { bins.nQuantiles }
     let yearMin: Float
+    let bins: Bins
     
-    static var binsPerYear: Int { 12 }
+    static var binsPerYear: Int { 4 }
     
     /// input temperature and time axis
     init(vector: ArraySlice<Float>, time: TimerangeDt, bins: Bins) {
@@ -219,7 +354,7 @@ struct CdfMonthly10YearSliding {
         let yearMin = Float(time.range.lowerBound.timeIntervalSince1970 / 3600) / 24 / 365.25
         let nYears = Int(yearMax - yearMin) + 2
         
-        let count = bins.count
+        let count = bins.nQuantiles
         var cdf = [Float](repeating: 0, count: count * Self.binsPerYear * nYears)
         for (t, value) in zip(time, vector) {
             let fractionalDayOfYear = ((t.timeIntervalSince1970 % 31_557_600) + 31_557_600) % 31_557_600
@@ -232,8 +367,8 @@ struct CdfMonthly10YearSliding {
             for (i, bin) in bins.enumerated().reversed() {
                 if value < bin {
                     for y in max(yearBin-5, 0) ..< min(yearBin+5+1, nYears) {
-                        cdf[(monthBin * count + i) + count * Self.binsPerYear * y] += fraction
-                        cdf[(((monthBin+1) % Self.binsPerYear) * count + i) + count * Self.binsPerYear * y] += 1-fraction
+                        cdf[(monthBin * count + i) + count * Self.binsPerYear * y] += 1-fraction
+                        cdf[(((monthBin+1) % Self.binsPerYear) * count + i) + count * Self.binsPerYear * y] += fraction
                         //cdf[(monthBin * count + i) + count * binsPerYear * y] += 1
                         //cdf[(((monthBin+1) % binsPerYear) * count + i) + count * binsPerYear * y] += 1
                         //cdf[(((monthBin-1+binsPerYear) % binsPerYear) * count + i) + count * binsPerYear * y] += 1
@@ -252,7 +387,7 @@ struct CdfMonthly10YearSliding {
             }
         }
         self.cdf = cdf
-        self.binLength = count
+        self.bins = bins
         self.yearMin = yearMin
     }
     
@@ -266,7 +401,21 @@ struct CdfMonthly10YearSliding {
         let fractionalYear = Float(t.timeIntervalSince1970 / 3600) / 24 / 365.25
         let yearBin = Int(fractionalYear - yearMin)
         
-        return cdf[binLength * monthBin + binLength * Self.binsPerYear * yearBin ..< binLength * (monthBin+1)  + binLength * Self.binsPerYear * yearBin]
+        return cdf[nBins * monthBin + nBins * Self.binsPerYear * yearBin ..< nBins * (monthBin+1)  + nBins * Self.binsPerYear * yearBin]
+    }
+    
+    /// linear interpolate between 2 months CDF
+    func get(bin: Int, time t: Timestamp) -> Float {
+        // TODO: interpolate between two CDFs... maybe do it upstream to prevent array allocations
+        
+        let fractionalDayOfYear = ((t.timeIntervalSince1970 % 31_557_600) + 31_557_600) % 31_557_600
+        let monthBin = fractionalDayOfYear / (31_557_600 / Self.binsPerYear)
+        let fraction = Float(fractionalDayOfYear).truncatingRemainder(dividingBy: Float(31_557_600 / Self.binsPerYear)) / Float(31_557_600 / Self.binsPerYear)
+        
+        let fractionalYear = Float(t.timeIntervalSince1970 / 3600) / 24 / 365.25
+        let yearBin = Int(fractionalYear - yearMin)
+        
+        return cdf[nBins * monthBin + nBins * Self.binsPerYear * yearBin + bin] * (1-fraction) + cdf[nBins * ((monthBin+1) % Self.binsPerYear) + nBins * Self.binsPerYear * yearBin + bin + 1] * (fraction)
     }
 }
 
