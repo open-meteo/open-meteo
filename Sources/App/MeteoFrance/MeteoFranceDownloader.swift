61466c7212254763848364189d33dd53e499b403
patrick-zippenfenig <patrick@zippenfenig.de>
2023-04-11 10:09:53 +0200

    increase arpege download timeout

--- Sources/App/MeteoFrance/MeteoFranceDownloader.swift
+++ Sources/App/MeteoFrance/MeteoFranceDownloader.swift
@@ -128,6 +128,8 @@ struct MeteoFranceDownload: AsyncCommandFix {
     /// download MeteoFrance
     func download(application: Application, domain: MeteoFranceDomain, run: Timestamp, variables: [MeteoFranceVariableDownloadable], skipFilesIfExisting: Bool) async throws {
         let logger = application.logger
+        /// Up to 7 hours download times are possible for arpege europe 12z run, after Meteofrance open-data limitations on the 12. February 2023
+        let deadLineHours = domain == .arpege_europe && run.hour == 12 ? 7 : 5
         let curl = Curl(logger: logger, client: application.dedicatedHttpClient, deadLineHours: 5)
                 
         /// world 0-24, 27-48, 51-72, 75-102
