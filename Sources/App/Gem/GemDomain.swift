17cf1733f686bdc4bc825c13218167cc7060fef6
patrick-zippenfenig <patrick@zippenfenig.de>
2023-04-28 15:39:17 +0200

    refactor GEM to multiple files

---
+++ Sources/App/Gem/GemDomain.swift
@@ -0,0 +1,184 @@
+import Foundation
+import SwiftPFor2D
+
+
+/**
+ Definition of GEM domains from the Canadian Weather Service
+ */
+enum GemDomain: String, GenericDomain, CaseIterable {
+    case gem_global
+    case gem_regional
+    case gem_hrdps_continental
+    case gem_global_ensemble
+    
+    var omfileDirectory: String {
+        return "\(OpenMeteo.dataDictionary)omfile-\(rawValue)/"
+    }
+    var downloadDirectory: String {
+        return "\(OpenMeteo.dataDictionary)download-\(rawValue)/"
+    }
+    var omfileArchive: String? {
+        return nil
+    }
+    var omFileMaster: (path: String, time: TimerangeDt)? {
+        return nil
+    }
+    
+    var dtSeconds: Int {
+        switch self {
+        case .gem_global:
+            return 3*3600
+        case .gem_regional:
+            return 3600
+        case .gem_hrdps_continental:
+            return 3600
+        case .gem_global_ensemble:
+            return 3*3600
+        }
+    }
+    var isGlobal: Bool {
+        switch self {
+        case .gem_global:
+            return true
+        case .gem_regional:
+            return false
+        case .gem_hrdps_continental:
+            return false
+        case .gem_global_ensemble:
+            return true
+        }
+    }
+
+    private static var gemGlobalElevationFile = try? OmFileReader(file: Self.gem_global.surfaceElevationFileOm)
+    private static var gemRegionalElevationFile = try? OmFileReader(file: Self.gem_regional.surfaceElevationFileOm)
+    private static var gemHrdpsContinentalElevationFile = try? OmFileReader(file: Self.gem_hrdps_continental.surfaceElevationFileOm)
+    private static var gemGlobalEnsembleElevationFile = try? OmFileReader(file: Self.gem_global_ensemble.surfaceElevationFileOm)
+    
+    func getStaticFile(type: ReaderStaticVariable) -> OmFileReader<MmapFile>? {
+        switch type {
+        case .soilType:
+            return nil
+        case .elevation:
+            switch self {
+            case .gem_global:
+                return Self.gemGlobalElevationFile
+            case .gem_regional:
+                return Self.gemRegionalElevationFile
+            case .gem_hrdps_continental:
+                return Self.gemHrdpsContinentalElevationFile
+            case .gem_global_ensemble:
+                return Self.gemGlobalEnsembleElevationFile
+            }
+        }
+    }
+    
+    /// Based on the current time , guess the current run that should be available soon on the open-data server
+    var lastRun: Timestamp {
+        let t = Timestamp.now()
+        switch self {
+        case .gem_global:
+            // First hours 3:40 h delay, second part 6.5 h delay
+            // every 12 hours
+            return t.add(-3*3600).floor(toNearest: 12*3600)
+        case .gem_regional:
+            // Delay of 2:47 hours to init
+            // every 6 hours
+            return t.add(-2*3600).floor(toNearest: 6*3600)
+        case .gem_hrdps_continental:
+            // Delay of 3:08 hours to init
+            // every 6 hours
+            return t.add(-2*3600).floor(toNearest: 6*3600)
+        case .gem_global_ensemble:
+            return t.add(-3*3600).floor(toNearest: 12*3600)
+        }
+    }
+    
+    /// Filename of the surface elevation file
+    var surfaceElevationFileOm: String {
+        "\(omfileDirectory)HSURF.om"
+    }
+    
+    func getForecastHours(run: Timestamp) -> [Int] {
+        switch self {
+        case .gem_global:
+            return Array(stride(from: 0, through: 240, by: 3))
+        case .gem_regional:
+            return Array(stride(from: 0, through: 84, by: 1))
+        case .gem_hrdps_continental:
+            return Array(stride(from: 0, through: 48, by: 1))
+        case .gem_global_ensemble:
+            let through = run.weekday == .thursday ? 768 : 384
+            return Array(stride(from: 0, to: 192, by: 3)) + Array(stride(from: 192, through: through, by: 6))
+        }
+    }
+    
+    /// pressure levels
+    var levels: [Int] {
+        switch self {
+        case .gem_global:
+            fallthrough
+        case .gem_regional:
+            return [1015, 1000, 985, 970, 950, 925, 900, 875, 850, 800, 750, 700, 650, 600, 550, 500, 450, 400, 350, 300, 275, 250, 225, 200, 175, 150, 100, 50, 30, 20, 10/*, 5, 1*/].reversed() // 5 and 1 not available for dewpoint
+        case .gem_hrdps_continental:
+            return [1015, 1000, 985, 970, 950, 925, 900, 875, 850, 800, 750, 700, 650, 600, 550, 500, 450, 400, 350, 300, 275, 250, 225, 200, 175, 150, 100, 50].reversed()
+        case .gem_global_ensemble:
+            /// Smaller selection, same as ECMWF IFS04
+            return [50, 200, 250, 300, 500, 700, 850, 925, 1000]
+        }
+    }
+    
+    var ensembleMembers: Int {
+        switch self {
+        case .gem_global:
+            return 1
+        case .gem_regional:
+            return 1
+        case .gem_hrdps_continental:
+            return 1
+        case .gem_global_ensemble:
+            return 20+1
+        }
+    }
+    
+    func getUrl(run: Timestamp, hour: Int, gribName: String) -> String {
+        let h3 = hour.zeroPadded(len: 3)
+        let yyyymmddhh = run.format_YYYYMMddHH
+        let server = "https://hpfx.collab.science.gc.ca/\(run.format_YYYYMMdd)/WXO-DD/"
+        switch self {
+        case .gem_global:
+            return "\(server)model_gem_global/15km/grib2/lat_lon/\(run.hh)/\(h3)/CMC_glb_\(gribName)_latlon.15x.15_\(yyyymmddhh)_P\(h3).grib2"
+        case .gem_regional:
+            return "\(server)model_gem_regional/10km/grib2/\(run.hh)/\(h3)/CMC_reg_\(gribName)_ps10km_\(yyyymmddhh)_P\(h3).grib2"
+        case .gem_hrdps_continental:
+            return "\(server)model_hrdps/continental/2.5km/\(run.hh)/\(h3)/\(run.format_YYYYMMdd)T\(run.hh)Z_MSC_HRDPS_\(gribName)_RLatLon0.0225_PT\(h3)H.grib2"
+        case .gem_global_ensemble:
+            return "\(server)ensemble/geps/grib2/raw/\(run.hh)/\(h3)/CMC_geps-raw_\(gribName)_latlon0p5x0p5_\(yyyymmddhh)_P\(h3)_allmbrs.grib2"
+        }
+    }
+    
+    var omFileLength: Int {
+        switch self {
+        case .gem_global:
+            return 110
+        case .gem_regional:
+            return 78+36
+        case .gem_hrdps_continental:
+            return 48+36
+        case .gem_global_ensemble:
+            return 384/3 + 48/3 // 144
+        }
+    }
+    
+    var grid: Gridable {
+        switch self {
+        case .gem_global:
+            return RegularGrid(nx: 2400, ny: 1201, latMin: -90, lonMin: -180, dx: 0.15, dy: 0.15)
+        case .gem_regional:
+            return ProjectionGrid(nx: 935, ny: 824, latitude: 18.14503...45.405453, longitude: 217.10745...349.8256, projection: StereograpicProjection(latitude: 90, longitude: 249, radius: 6371229))
+        case .gem_hrdps_continental:
+            return ProjectionGrid(nx: 2540, ny: 1290, latitude: 39.626034...47.876457, longitude: -133.62952...(-40.708557), projection: RotatedLatLonProjection(latitude: -36.0885, longitude: 245.305))
+        case .gem_global_ensemble:
+            return RegularGrid(nx: 720, ny: 361, latMin: -90, lonMin: -180, dx: 0.5, dy: 0.5)
+        }
+    }
+}
---
+++ Sources/App/Gem/GemVariable.swift
@@ -0,0 +1,565 @@
+import Foundation
+
+/**
+ Protocol to define meta information to download
+ */
+protocol GemVariableDownloadable: GenericVariable {
+    func multiplyAdd(dtSeconds: Int) -> (multiply: Float, add: Float)?
+    var skipHour0: Bool { get }
+    func includedFor(hour: Int, domain: GemDomain) -> Bool
+    func gribName(domain: GemDomain) -> String?
+    var isAccumulatedSinceModelStart: Bool { get }
+}
+
+/**
+ List of available GEM variables to download
+ */
+enum GemSurfaceVariable: String, CaseIterable, GemVariableDownloadable, GenericVariableMixable {
+    case temperature_2m
+    case temperature_40m
+    case temperature_80m
+    case temperature_120m
+    case relativehumidity_2m
+    case cloudcover
+    case pressure_msl
+    
+    case shortwave_radiation
+    
+    case wind_u_component_10m
+    case wind_v_component_10m
+    case wind_u_component_40m
+    case wind_v_component_40m
+    case wind_u_component_80m
+    case wind_v_component_80m
+    case wind_u_component_120m
+    case wind_v_component_120m
+    
+    /// there is also min/max
+    case windgusts_10m
+    
+    case showers
+    
+    case snowfall_water_equivalent
+    
+    case soil_temperature_0_to_10cm
+    case soil_moisture_0_to_10cm
+    
+    
+    /// accumulated since forecast start `kg m-2 sec-1`
+    case precipitation
+    
+    case cape
+    
+    //case cin
+    
+    //case lifted_index
+    
+    func gribName(domain: GemDomain) -> String? {
+        switch domain {
+        case .gem_global:
+            fallthrough
+        case .gem_regional:
+            switch self {
+            case .temperature_2m:
+                return "TMP_TGL_2"
+            case .temperature_40m:
+                return "TMP_TGL_40"
+            case .temperature_80m:
+                return "TMP_TGL_80"
+            case .temperature_120m:
+                return "TMP_TGL_120"
+            case .wind_u_component_10m:
+                return "UGRD_TGL_10"
+            case .wind_v_component_10m:
+                return "VGRD_TGL_10"
+            case .wind_u_component_40m:
+                return "UGRD_TGL_40"
+            case .wind_v_component_40m:
+                return "VGRD_TGL_40"
+            case .wind_u_component_80m:
+                return "UGRD_TGL_80"
+            case .wind_v_component_80m:
+                return "VGRD_TGL_80"
+            case .wind_u_component_120m:
+                return "UGRD_TGL_120"
+            case .wind_v_component_120m:
+                return "VGRD_TGL_120"
+            case .relativehumidity_2m:
+                return "RH_TGL_2"
+            case .showers:
+                return "ACPCP_SFC_0"
+            case .cloudcover:
+                return "TCDC_SFC_0"
+            case .pressure_msl:
+                return "PRMSL_MSL_0"
+            case .shortwave_radiation:
+                return "DSWRF_SFC_0"
+            case .windgusts_10m:
+                return "GUST_TGL_10"
+            case .precipitation:
+                return "APCP_SFC_0"
+            case .snowfall_water_equivalent:
+                return "WEASN_SFC_0"
+            case .cape:
+                return "CAPE_SFC_0"
+            //case .cin:
+            //    return "CIN_SFC_0"
+            //case .lifted_index:
+            //    return "4LFTX_SFC_0"
+            case .soil_temperature_0_to_10cm:
+                return "TSOIL_SFC_0"
+            case .soil_moisture_0_to_10cm:
+                return "SOILW_DBLY_10"
+            }
+        case .gem_hrdps_continental:
+            switch self {
+            case .temperature_2m:
+                return "TMP_AGL-2m"
+            case .temperature_40m:
+                return "TMP_AGL-40m"
+            case .temperature_80m:
+                return "TMP_AGL-80m"
+            case .temperature_120m:
+                return "TMP_AGL-120m"
+            case .relativehumidity_2m:
+                return "RH_AGL-2m"
+            case .cloudcover:
+                return "TCDC_Sfc"
+            case .pressure_msl:
+                return "PRMSL_MSL"
+            case .shortwave_radiation:
+                return "DSWRF_Sfc"
+            case .wind_u_component_10m:
+                return "UGRD_AGL-10m"
+            case .wind_v_component_10m:
+                return "VGRD_AGL-10m"
+            case .wind_u_component_40m:
+                return "UGRD_AGL-40m"
+            case .wind_v_component_40m:
+                return "VGRD_AGL-40m"
+            case .wind_u_component_80m:
+                return "UGRD_AGL-80m"
+            case .wind_v_component_80m:
+                return "VGRD_AGL-80m"
+            case .wind_u_component_120m:
+                return "UGRD_AGL-120m"
+            case .wind_v_component_120m:
+                return "VGRD_AGL-120m"
+            case .windgusts_10m:
+                return "GUST_AGL-10m"
+            case .showers:
+                return "ACPCP_Sfc"
+            case .snowfall_water_equivalent:
+                return "WEASN_Sfc"
+            case .soil_temperature_0_to_10cm:
+                return "TSOIL_DBS-0-10cm"
+            case .soil_moisture_0_to_10cm:
+                return "SOILW_DBS-0-10cm"
+            case .precipitation:
+                return "APCP_Sfc"
+            case .cape:
+                return "CAPE_Sfc"
+            }
+        case .gem_global_ensemble:
+            switch self {
+            case .relativehumidity_2m:
+                return "RH_TGL_2m"
+            case .showers:
+                return nil
+            case .wind_u_component_10m:
+                return "UGRD_TGL_10m"
+            case .wind_v_component_10m:
+                return "VGRD_TGL_10m"
+            case .wind_u_component_40m:
+                return nil
+            case .wind_v_component_40m:
+                return nil
+            case .wind_u_component_80m:
+                return nil
+            case .wind_v_component_80m:
+                return nil
+            case .wind_u_component_120m:
+                return nil
+            case .wind_v_component_120m:
+                return nil
+            case .temperature_2m:
+                return "TMP_TGL_2m"
+            case .temperature_40m:
+                return nil
+            case .temperature_80m:
+                return nil
+            case .temperature_120m:
+                return nil
+            case .snowfall_water_equivalent:
+                return "ASNOW_SFC_0"
+            case .soil_temperature_0_to_10cm:
+                return nil
+            case .soil_moisture_0_to_10cm:
+                return nil
+            case .cloudcover:
+                return "TCDC_SFC_0"
+            case .pressure_msl:
+                return "PRMSL_MSL_0"
+            case .shortwave_radiation:
+                return "DSWRF_SFC_0"
+            case .precipitation:
+                return "APCP_SFC_0"
+            case .cape:
+                return "CAPE_SFC_0"
+            case .windgusts_10m:
+                return nil
+            }
+        }
+    }
+    
+    func includedFor(hour: Int, domain: GemDomain) -> Bool {
+        if self == .cape && hour >= 171 {
+            return false
+        }
+        return true
+    }
+    
+    var isAccumulatedSinceModelStart: Bool {
+        switch self {
+        case .shortwave_radiation:
+            fallthrough
+        case .precipitation:
+            fallthrough
+        case .showers:
+            fallthrough
+        case .snowfall_water_equivalent:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    var omFileName: String {
+        return rawValue
+    }
+    
+    var scalefactor: Float {
+        switch self {
+        case .temperature_2m:
+            return 20
+        case .cloudcover:
+            return 1
+        case .precipitation:
+            return 10
+        case .pressure_msl:
+            return 10
+        case .wind_u_component_10m:
+            fallthrough
+        case .wind_v_component_10m:
+            fallthrough
+        case .wind_u_component_40m:
+            fallthrough
+        case .wind_v_component_40m:
+            fallthrough
+        case .wind_u_component_80m:
+            fallthrough
+        case .wind_v_component_80m:
+            fallthrough
+        case .wind_u_component_120m:
+            fallthrough
+        case .wind_v_component_120m:
+            return 10
+        case .soil_temperature_0_to_10cm:
+            return 20
+        case .soil_moisture_0_to_10cm:
+            return 1000
+        case .shortwave_radiation:
+            return 1
+        case .temperature_40m:
+            return 20
+        case .temperature_80m:
+            return 20
+        case .temperature_120m:
+            return 20
+        case .relativehumidity_2m:
+            return 1
+        case .windgusts_10m:
+            return 10
+        case .showers:
+            return 10
+        case .snowfall_water_equivalent:
+            return 10
+        case .cape:
+            return 0.1
+        }
+    }
+    
+    
+    func multiplyAdd(dtSeconds: Int) -> (multiply: Float, add: Float)? {
+        switch self {
+        case .temperature_2m:
+            fallthrough
+        case .temperature_40m:
+            fallthrough
+        case .temperature_80m:
+            fallthrough
+        case .temperature_120m:
+            fallthrough
+        case .soil_temperature_0_to_10cm:
+            return (1, -273.15)
+        case .pressure_msl:
+            return (1/100, 0)
+        case .shortwave_radiation:
+            return (1/Float(dtSeconds), 0) // joules to watt
+        default:
+            return nil
+        }
+    }
+    
+    var interpolation: ReaderInterpolation {
+        switch self {
+        case .temperature_2m:
+            return .hermite(bounds: nil)
+        case .cloudcover:
+            return .hermite(bounds: 0...100)
+        case .pressure_msl:
+            return .hermite(bounds: nil)
+        case .precipitation:
+            return .linear
+        case .shortwave_radiation:
+            return .solar_backwards_averaged
+        case .temperature_40m:
+            return .hermite(bounds: nil)
+        case .temperature_80m:
+            return .hermite(bounds: nil)
+        case .temperature_120m:
+            return .hermite(bounds: nil)
+        case .relativehumidity_2m:
+            return .hermite(bounds: 0...100)
+        case .wind_u_component_10m:
+            fallthrough
+        case .wind_v_component_10m:
+            fallthrough
+        case .wind_u_component_40m:
+            fallthrough
+        case .wind_v_component_40m:
+            fallthrough
+        case .wind_u_component_80m:
+            fallthrough
+        case .wind_v_component_80m:
+            fallthrough
+        case .wind_u_component_120m:
+            fallthrough
+        case .wind_v_component_120m:
+            return .hermite(bounds: nil)
+        case .windgusts_10m:
+            return .hermite(bounds: nil)
+        case .showers:
+            return .backwards_sum
+        case .snowfall_water_equivalent:
+            return .backwards_sum
+        case .soil_temperature_0_to_10cm:
+            return .hermite(bounds: nil)
+        case .soil_moisture_0_to_10cm:
+            return .hermite(bounds: nil)
+        case .cape:
+            return .hermite(bounds: 0...10e9)
+        }
+    }
+    
+    var unit: SiUnit {
+        switch self {
+        case .temperature_2m:
+            return .celsius
+        case .cloudcover:
+            return .percent
+        case .precipitation:
+            return .millimeter
+        case .pressure_msl:
+            return .hectoPascal
+        case .shortwave_radiation:
+            return .wattPerSquareMeter
+        case .temperature_40m:
+            return .celsius
+        case .temperature_80m:
+            return .celsius
+        case .temperature_120m:
+            return .celsius
+        case .relativehumidity_2m:
+            return .percent
+        case .wind_u_component_10m:
+            fallthrough
+        case .wind_v_component_10m:
+            fallthrough
+        case .wind_u_component_40m:
+            fallthrough
+        case .wind_v_component_40m:
+            fallthrough
+        case .wind_u_component_80m:
+            fallthrough
+        case .wind_v_component_80m:
+            fallthrough
+        case .wind_u_component_120m:
+            fallthrough
+        case .wind_v_component_120m:
+            fallthrough
+        case .windgusts_10m:
+            return .ms
+        case .showers:
+            return .millimeter
+        case .snowfall_water_equivalent:
+            return .millimeter
+        case .soil_temperature_0_to_10cm:
+            return .celsius
+        case .soil_moisture_0_to_10cm:
+            return .qubicMeterPerQubicMeter
+        case .cape:
+            return .joulesPerKilogram
+        }
+    }
+    
+    var isElevationCorrectable: Bool {
+        return self == .temperature_2m
+    }
+    
+    var requiresOffsetCorrectionForMixing: Bool {
+        return self == .soil_moisture_0_to_10cm
+    }
+    
+    var skipHour0: Bool {
+        switch self {
+        case .precipitation: return true
+        case .showers: return true
+        case .snowfall_water_equivalent: return true
+        case .shortwave_radiation: return true
+        default: return false
+        }
+    }
+}
+
+/**
+ Types of pressure level variables
+ */
+enum GemPressureVariableType: String, CaseIterable {
+    case temperature
+    case wind_u_component
+    case wind_v_component
+    case geopotential_height
+    case relativehumidity
+}
+
+
+/**
+ A pressure level variable on a given level in hPa / mb
+ */
+struct GemPressureVariable: PressureVariableRespresentable, GemVariableDownloadable, Hashable, GenericVariableMixable {
+    let variable: GemPressureVariableType
+    let level: Int
+    
+    var requiresOffsetCorrectionForMixing: Bool {
+        return false
+    }
+    
+    var omFileName: String {
+        return rawValue
+    }
+    func gribName(domain: GemDomain) -> String? {
+        let isbl = (domain == .gem_hrdps_continental || domain == .gem_global_ensemble) ? "ISBL_\(level.zeroPadded(len: 4))" : "ISBL_\(level)"
+        switch variable {
+        case .temperature:
+            return "TMP_\(isbl)"
+        case .wind_u_component:
+            return "UGRD_\(isbl)"
+        case .wind_v_component:
+            return "VGRD_\(isbl)"
+        case .geopotential_height:
+            return "HGT_\(isbl)"
+        case .relativehumidity:
+            return "RH_\(isbl)"
+        }
+    }
+    
+    func includedFor(hour: Int, domain: GemDomain) -> Bool {
+        if domain == .gem_global_ensemble {
+            // temperature and RH is missing for level 300 hpa
+            if (variable == .temperature || variable == .relativehumidity) && level == 300 {
+                return false
+            }
+            return true
+        }
+        if hour >= 171 && ![1000, 925, 850, 700, 500, 5, 1].contains(level) {
+            return false
+        }
+        return true
+    }
+    
+    var scalefactor: Float {
+        // Upper level data are more dynamic and that is bad for compression. Use lower scalefactors
+        switch variable {
+        case .temperature:
+            // Use scalefactor of 2 for everything higher than 300 hPa
+            return (2..<10).interpolated(atFraction: (300..<1000).fraction(of: Float(level)))
+        case .relativehumidity:
+            return (0.2..<1).interpolated(atFraction: (0..<800).fraction(of: Float(level)))
+        case .wind_u_component:
+            fallthrough
+        case .wind_v_component:
+            // Use scalefactor 3 for levels higher than 500 hPa.
+            return (3..<10).interpolated(atFraction: (500..<1000).fraction(of: Float(level)))
+        case .geopotential_height:
+            return (0.05..<1).interpolated(atFraction: (0..<500).fraction(of: Float(level)))
+        }
+    }
+    
+    var isAccumulatedSinceModelStart: Bool {
+        return false
+    }
+    
+    var interpolation: ReaderInterpolation {
+        switch variable {
+        case .temperature:
+            return .hermite(bounds: nil)
+        case .relativehumidity:
+            return .hermite(bounds: 0...100)
+        case .wind_u_component:
+            return .hermite(bounds: nil)
+        case .wind_v_component:
+            return .hermite(bounds: nil)
+        case .geopotential_height:
+            return .hermite(bounds: nil)
+        }
+    }
+    
+    func multiplyAdd(dtSeconds: Int) -> (multiply: Float, add: Float)? {
+        switch variable {
+        case .temperature:
+            return (1, -273.15)
+        default:
+            return nil
+        }
+    }
+    
+    var unit: SiUnit {
+        switch variable {
+        case .temperature:
+            return .celsius
+        case .wind_u_component:
+            return .ms
+        case .wind_v_component:
+            return .ms
+        case .geopotential_height:
+            return .meter
+        case .relativehumidity:
+            return .percent
+        }
+    }
+    
+    var isElevationCorrectable: Bool {
+        return false
+    }
+    
+    var skipHour0: Bool {
+        return false
+    }
+}
+
+/**
+ Combined surface and pressure level variables with all definitions for downloading and API
+ */
+typealias GemVariable = SurfaceAndPressureVariable<GemSurfaceVariable, GemPressureVariable>
+
