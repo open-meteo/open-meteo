838ecbf1ef3e6b2a2edc792a2af6edc29b64a351
patrick-zippenfenig <patrick@zippenfenig.de>
2023-09-01 15:51:33 +0200

    increase CFS download timeout

--- Sources/App/SeasonalForecast/SeasonalForecastDownload.swift
+++ Sources/App/SeasonalForecast/SeasonalForecastDownload.swift
@@ -80,7 +80,7 @@ struct SeasonalForecastDownload: AsyncCommandFix {
         let logger = application.logger
         try FileManager.default.createDirectory(atPath: domain.downloadDirectory, withIntermediateDirectories: true)
         
-        let curl = Curl(logger: logger, client: application.dedicatedHttpClient)
+        let curl = Curl(logger: logger, client: application.dedicatedHttpClient, deadLineHours: 18, readTimeout: 20*60)
         
         let gribVariables = ["tmp2m", "tmin", "soilt1", "dswsfc", "cprat", "q2m", "wnd10m", "tcdcclm", "prate", "soilm3", "pressfc", "soilm2", "soilm1", "soilm4", "tmax"]
         
