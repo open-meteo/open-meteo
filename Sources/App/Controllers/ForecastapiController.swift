1b3b7dfb09c989e1a72b4744cb250c7db4af4df2
patrick-zippenfenig <patrick@zippenfenig.de>
2023-09-26 12:55:23 +0200

    json conversion now working

--- Sources/App/Controllers/ForecastapiController.swift
+++ Sources/App/Controllers/ForecastapiController.swift
@@ -48,7 +48,7 @@ public struct ForecastapiController: RouteCollection {
         let nParamsMinutely = paramsMinutely?.count ?? 0
         let nParamsCurrent = paramsCurrent?.count ?? 0
         let nParamsDaily = paramsDaily?.count ?? 0
-        let nVariables = (nParamsHourly + nParamsMinutely + nParamsCurrent + nParamsDaily) * domains.count
+        let nVariables = nParamsHourly + nParamsMinutely + nParamsCurrent + nParamsDaily
         
         let locations: [ForecastapiResultMulti<MultiDomains, ForecastVariable, ForecastVariableDaily>] = try prepared.map { prepared in
             let coordinates = prepared.coordinate
