b22663e6b9697770064626eb8e5eb2afc9c221dc
patrick-zippenfenig <patrick@zippenfenig.de>
2023-10-24 11:22:57 +0200

    last missing cases

--- Sources/App/Helper/FlatBufferWriter/FlatBuffers+MarineApi.swift
+++ Sources/App/Helper/FlatBufferWriter/FlatBuffers+MarineApi.swift
@@ -6,6 +6,28 @@ import OpenMeteoSdk
 extension IconWaveVariable: FlatBuffersVariable {
     func getFlatBuffersMeta() -> FlatBufferVariableMeta {
         switch self {
+        case .wave_height:
+            return .init(variable: .waveHeight)
+        case .wave_period:
+            return .init(variable: .wavePeriod)
+        case .wave_direction:
+            return .init(variable: .waveDirection)
+        case .wind_wave_height:
+            return .init(variable: .windWaveHeight)
+        case .wind_wave_period:
+            return .init(variable: .windWavePeriod)
+        case .wind_wave_peak_period:
+            return .init(variable: .windWavePeakPeriod)
+        case .wind_wave_direction:
+            return .init(variable: .windWaveDirection)
+        case .swell_wave_height:
+            return .init(variable: .swellWaveHeight)
+        case .swell_wave_period:
+            return .init(variable: .swellWavePeriod)
+        case .swell_wave_peak_period:
+            return .init(variable: .swellWavePeakPeriod)
+        case .swell_wave_direction:
+            return .init(variable: .swellWaveDirection)
         }
     }
 }
@@ -13,6 +35,28 @@ extension IconWaveVariable: FlatBuffersVariable {
 extension IconWaveVariableDaily: FlatBuffersVariable {
     func getFlatBuffersMeta() -> FlatBufferVariableMeta {
         switch self {
+        case .wave_height_max:
+            return .init(variable: .windWaveHeight, aggregation: .maximum)
+        case .wind_wave_height_max:
+            return .init(variable: .windWaveHeight, aggregation: .maximum)
+        case .swell_wave_height_max:
+            return .init(variable: .swellWaveHeight, aggregation: .maximum)
+        case .wave_direction_dominant:
+            return .init(variable: .waveDirection, aggregation: .dominant)
+        case .wind_wave_direction_dominant:
+            return .init(variable: .windWaveDirection, aggregation: .dominant)
+        case .swell_wave_direction_dominant:
+            return .init(variable: .swellWaveDirection, aggregation: .dominant)
+        case .wave_period_max:
+            return .init(variable: .wavePeriod, aggregation: .maximum)
+        case .wind_wave_period_max:
+            return .init(variable: .windWavePeriod, aggregation: .maximum)
+        case .wind_wave_peak_period_max:
+            return .init(variable: .windWavePeakPeriod, aggregation: .maximum)
+        case .swell_wave_period_max:
+            return .init(variable: .swellWavePeriod, aggregation: .maximum)
+        case .swell_wave_peak_period_max:
+            return .init(variable: .swellWavePeakPeriod, aggregation: .maximum)
         }
     }
 }
