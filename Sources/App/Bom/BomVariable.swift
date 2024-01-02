8b84e1ec97ba432ebfc565940b5f516708f4872e
patrick-zippenfenig <patrick@zippenfenig.de>
2024-01-02 12:40:33 +0100

    bom variables init

--- Sources/App/Bom/BomVariable.swift
+++ Sources/App/Bom/BomVariable.swift
@@ -6,66 +6,82 @@ protocol BomVariableDownloadable: GenericVariable {
 }
 
 enum BomSurfaceVariable: String, CaseIterable, GenericVariableMixable, BomVariableDownloadable {
+    // ml has 1h delay! ml analysis has a lot of levels!
+    // ml temp: 20 53.3 100 160
+    // ml wind: 10 36.6 76.6 130
+    
+    case showers
+    case precipitation
+    case pressure_msl
+    case direct_radiation
+    case shortwave_radiation
     case temperature_2m
+    case relative_humidity_2m
     case cloud_cover
     case cloud_cover_low
     case cloud_cover_mid
     case cloud_cover_high
-    case pressure_msl
-    case relative_humidity_2m
-    
-    case shortwave_radiation
-    case shortwave_radiation_clear_sky
+    case surface_temperature
+    case snow_depth
     
     case wind_v_component_10m
     case wind_u_component_10m
-    case wind_v_component_30m
-    case wind_u_component_30m
-    case wind_v_component_50m
-    case wind_u_component_50m
-    case wind_v_component_70m
-    case wind_u_component_70m
-    case wind_v_component_100m
-    case wind_u_component_100m
-    case wind_v_component_120m
-    case wind_u_component_120m
-    case wind_v_component_140m
-    case wind_u_component_140m
-    case wind_v_component_160m
-    case wind_u_component_160m
-    case wind_v_component_180m
-    case wind_u_component_180m
-    case wind_v_component_200m
-    case wind_u_component_200m
+    
+    case soil_temperature_10cm
+    case soil_temperature_35cm
+    case soil_temperature_100cm
+    case soil_temperature_300cm
+    
+    case soil_moisture_10cm
+    case soil_moisture_35cm
+    case soil_moisture_100cm
+    case soil_moisture_300cm
+    
+    case visibility
+    //case thunderstorm_probability // bright et all 0-1000K
+    
     case wind_gusts_10m
     
-    case precipitation
-    case precipitation_type
-    case showers
-    case snowfall
-    case surface_temperature
-    case snow_depth
-    case cape
-    case convective_inhibition
-    case lifted_index
-    case visibility
+    //case snow_large_scale
+    //case snow_showers
     
-    case soil_temperature_0_to_10cm
-    case soil_temperature_10_to_40cm
-    case soil_temperature_40_to_100cm
-    case soil_temperature_100_to_200cm
-    
-    case soil_moisture_0_to_10cm
-    case soil_moisture_10_to_40cm
-    case soil_moisture_40_to_100cm
-    case soil_moisture_100_to_200cm
+    var bomName: String {
+        switch self {
+        case .temperature_2m: "temp_scrn"
+        case .showers: "accum_conv_rain"
+        case .precipitation: "accum_prcp"
+        case .pressure_msl: "mslp" //Pa
+        case .direct_radiation: "av_sfc_sw_dir"
+        case .shortwave_radiation: "av_swsfcdown"
+        case .relative_humidity_2m: "rh_scrn" // %, BUT >117%
+        case .cloud_cover: "ttl_cld"
+        case .cloud_cover_high: "hi_cld" //0-1
+        case .cloud_cover_mid: "mid_cld"
+        case .cloud_cover_low: "low_cld"
+        case .surface_temperature: "sfc_temp"
+        case .snow_depth: "snow_amt_lnd" // kg/m2
+        case .soil_temperature_10cm: "soil_temp"
+        case .soil_temperature_35cm: "soil_temp2"
+        case .soil_temperature_100cm: "soil_temp3"
+        case .soil_temperature_300cm: "soil_temp4"
+        case .soil_moisture_10cm: "soil_mois"
+        case .soil_moisture_35cm: "soil_mois2"
+        case .soil_moisture_100cm: "soil_mois3"
+        case .soil_moisture_300cm: "soil_mois4"
+        case .wind_v_component_10m: "uwnd10m"
+        case .wind_u_component_10m: "vwnd10m"
+        case .visibility: "visibility"
+        case .wind_gusts_10m: "wndgust10m"
+        //case .thunderstorm_probability: "cld_phys_thunder_p"
+        }
+    }
     
     var requiresOffsetCorrectionForMixing: Bool {
         switch self {
-        case .soil_moisture_0_to_10cm: return true
-        case .soil_moisture_10_to_40cm: return true
-        case .soil_moisture_40_to_100cm: return true
-        case .soil_moisture_100_to_200cm: return true
+        case .soil_moisture_10cm: return true
+        case .soil_moisture_35cm: return true
+        case .soil_moisture_100cm: return true
+        case .soil_moisture_300cm: return true
         case .snow_depth: return true
         default: return false
         }
@@ -73,12 +89,12 @@ enum BomSurfaceVariable: String, CaseIterable, GenericVariableMixable, BomVariab
     
     var multiplyAdd: (multiply: Float, add: Float)? {
         switch self {
-        case .temperature_2m, .surface_temperature, .soil_temperature_0_to_10cm, .soil_temperature_10_to_40cm, .soil_temperature_40_to_100cm, .soil_temperature_100_to_200cm:
+        case .temperature_2m, .surface_temperature, .soil_temperature_10cm, .soil_temperature_35cm, .soil_temperature_100cm, .soil_temperature_300cm:
             return (1, -273.15)
-        case .snowfall:
+        case .snow_depth:
+            return (0.7/100, 0)
+        case .cloud_cover, .cloud_cover_low, .cloud_cover_mid, .cloud_cover_high:
             return (100, 0)
-        case .shortwave_radiation, .shortwave_radiation_clear_sky:
-            return (1/1000, 0)
         case .pressure_msl:
             return (1/100, 0)
         default:
@@ -101,48 +117,23 @@ enum BomSurfaceVariable: String, CaseIterable, GenericVariableMixable, BomVariab
         case .precipitation: return 10
         case .wind_v_component_10m: return 10
         case .wind_u_component_10m: return 10
-        case .wind_v_component_30m: return 10
-        case .wind_u_component_30m: return 10
-        case .wind_v_component_50m: return 10
-        case .wind_u_component_50m: return 10
-        case .wind_v_component_70m: return 10
-        case .wind_u_component_70m: return 10
-        case .wind_u_component_100m: return 10
-        case .wind_v_component_100m: return 10
-        case .wind_v_component_120m: return 10
-        case .wind_u_component_120m: return 10
-        case .wind_v_component_140m: return 10
-        case .wind_u_component_140m: return 10
-        case .wind_u_component_160m: return 10
-        case .wind_v_component_160m: return 10
-        case .wind_u_component_180m: return 10
-        case .wind_v_component_180m: return 10
-        case .wind_u_component_200m: return 10
-        case .wind_v_component_200m: return 10
         case .surface_temperature: return 20
-        case .soil_temperature_0_to_10cm: return 20
-        case .soil_temperature_10_to_40cm: return 20
-        case .soil_temperature_40_to_100cm: return 20
-        case .soil_temperature_100_to_200cm: return 20
-        case .soil_moisture_0_to_10cm: return 1000
-        case .soil_moisture_10_to_40cm: return 1000
-        case .soil_moisture_40_to_100cm: return 1000
-        case .soil_moisture_100_to_200cm: return 1000
+        case .soil_temperature_10cm: return 20
+        case .soil_temperature_35cm: return 20
+        case .soil_temperature_100cm: return 20
+        case .soil_temperature_300cm: return 20
+        case .soil_moisture_10cm: return 1000
+        case .soil_moisture_35cm: return 1000
+        case .soil_moisture_100cm: return 1000
+        case .soil_moisture_300cm: return 1000
         case .snow_depth: return 100 // 1cm res
         case .wind_gusts_10m: return 10
         case .showers: return 10
         case .pressure_msl: return 10
         case .shortwave_radiation: return 1
-        case .cape: return 0.1
-        case .lifted_index: return 10
+        case .direct_radiation: return 1
         case .visibility: return 0.05 // 50 meter
-        case .convective_inhibition: return 1
-        case .shortwave_radiation_clear_sky:
-            return 1
-        case .precipitation_type:
-            return 1
-        case .snowfall:
-            return 10
+        //case .snowfall: return 10
         }
     }
     
@@ -168,59 +159,23 @@ enum BomSurfaceVariable: String, CaseIterable, GenericVariableMixable, BomVariab
             return .hermite(bounds: nil)
         case .wind_u_component_10m:
             return .hermite(bounds: nil)
-        case .wind_v_component_30m:
-            return .hermite(bounds: nil)
-        case .wind_u_component_30m:
-            return .hermite(bounds: nil)
-        case .wind_v_component_50m:
-            return .hermite(bounds: nil)
-        case .wind_u_component_50m:
-            return .hermite(bounds: nil)
-        case .wind_v_component_70m:
-            return .hermite(bounds: nil)
-        case .wind_u_component_70m:
-            return .hermite(bounds: nil)
-        case .wind_v_component_100m:
-            return .hermite(bounds: nil)
-        case .wind_u_component_100m:
-            return .hermite(bounds: nil)
-        case .wind_v_component_120m:
-            return .hermite(bounds: nil)
-        case .wind_u_component_120m:
-            return .hermite(bounds: nil)
-        case .wind_v_component_140m:
-            return .hermite(bounds: nil)
-        case .wind_u_component_140m:
-            return .hermite(bounds: nil)
-        case .wind_v_component_160m:
-            return .hermite(bounds: nil)
-        case .wind_u_component_160m:
-            return .hermite(bounds: nil)
-        case .wind_v_component_180m:
-            return .hermite(bounds: nil)
-        case .wind_u_component_180m:
-            return .hermite(bounds: nil)
-        case .wind_v_component_200m:
-            return .hermite(bounds: nil)
-        case .wind_u_component_200m:
-            return .hermite(bounds: nil)
         case .surface_temperature:
             return .hermite(bounds: nil)
-        case .soil_temperature_0_to_10cm:
+        case .soil_temperature_10cm:
             return .hermite(bounds: nil)
-        case .soil_temperature_10_to_40cm:
+        case .soil_temperature_35cm:
             return .hermite(bounds: nil)
-        case .soil_temperature_40_to_100cm:
+        case .soil_temperature_100cm:
             return .hermite(bounds: nil)
-        case .soil_temperature_100_to_200cm:
+        case .soil_temperature_300cm:
             return .hermite(bounds: nil)
-        case .soil_moisture_0_to_10cm:
+        case .soil_moisture_10cm:
             return .hermite(bounds: nil)
-        case .soil_moisture_10_to_40cm:
+        case .soil_moisture_35cm:
             return .hermite(bounds: nil)
-        case .soil_moisture_40_to_100cm:
+        case .soil_moisture_100cm:
             return .hermite(bounds: nil)
-        case .soil_moisture_100_to_200cm:
+        case .soil_moisture_300cm:
             return .hermite(bounds: nil)
         case .snow_depth:
             return .linear
@@ -228,22 +183,10 @@ enum BomSurfaceVariable: String, CaseIterable, GenericVariableMixable, BomVariab
             return .backwards_sum
         case .wind_gusts_10m:
             return .hermite(bounds: nil)
-        case .shortwave_radiation:
+        case .shortwave_radiation, .direct_radiation:
             return .solar_backwards_averaged
-        case .cape:
-            return .hermite(bounds: 0...10e9)
-        case .lifted_index:
-            return .hermite(bounds: nil)
         case .visibility:
             return .linear
-        case .convective_inhibition:
-            return .hermite(bounds: nil)
-        case .shortwave_radiation_clear_sky:
-            return .solar_backwards_averaged
-        case .precipitation_type:
-            return .backwards
-        case .snowfall:
-            return .backwards_sum
         }
     }
     
@@ -258,48 +201,21 @@ enum BomSurfaceVariable: String, CaseIterable, GenericVariableMixable, BomVariab
         case .precipitation: return .millimetre
         case .wind_v_component_10m: return .metrePerSecond
         case .wind_u_component_10m: return .metrePerSecond
-        case .wind_v_component_30m: return .metrePerSecond
-        case .wind_u_component_30m: return .metrePerSecond
-        case .wind_v_component_50m: return .metrePerSecond
-        case .wind_u_component_50m: return .metrePerSecond
-        case .wind_v_component_70m: return .metrePerSecond
-        case .wind_u_component_70m: return .metrePerSecond
-        case .wind_v_component_100m: return .metrePerSecond
-        case .wind_u_component_100m: return .metrePerSecond
-        case .wind_v_component_120m: return .metrePerSecond
-        case .wind_u_component_120m: return .metrePerSecond
-        case .wind_v_component_140m: return .metrePerSecond
-        case .wind_u_component_140m: return .metrePerSecond
-        case .wind_v_component_160m: return .metrePerSecond
-        case .wind_u_component_160m: return .metrePerSecond
-        case .wind_v_component_180m: return .metrePerSecond
-        case .wind_u_component_180m: return .metrePerSecond
-        case .wind_v_component_200m: return .metrePerSecond
-        case .wind_u_component_200m: return .metrePerSecond
         case .surface_temperature: return .celsius
-        case .soil_temperature_0_to_10cm: return .celsius
-        case .soil_temperature_10_to_40cm: return .celsius
-        case .soil_temperature_40_to_100cm: return .celsius
-        case .soil_temperature_100_to_200cm: return .celsius
-        case .soil_moisture_0_to_10cm: return .cubicMetrePerCubicMetre
-        case .soil_moisture_10_to_40cm: return .cubicMetrePerCubicMetre
-        case .soil_moisture_40_to_100cm: return .cubicMetrePerCubicMetre
-        case .soil_moisture_100_to_200cm: return .cubicMetrePerCubicMetre
+        case .soil_temperature_10cm: return .celsius
+        case .soil_temperature_35cm: return .celsius
+        case .soil_temperature_100cm: return .celsius
+        case .soil_temperature_300cm: return .celsius
+        case .soil_moisture_10cm: return .cubicMetrePerCubicMetre
+        case .soil_moisture_35cm: return .cubicMetrePerCubicMetre
+        case .soil_moisture_100cm: return .cubicMetrePerCubicMetre
+        case .soil_moisture_300cm: return .cubicMetrePerCubicMetre
         case .snow_depth: return .metre
         case .showers: return .millimetre
         case .wind_gusts_10m: return .metrePerSecond
         case .pressure_msl: return .hectopascal
-        case .shortwave_radiation: return .wattPerSquareMetre
-        case .cape: return .joulePerKilogram
-        case .lifted_index: return .dimensionless
+        case .shortwave_radiation, .direct_radiation: return .wattPerSquareMetre
         case .visibility: return .metre
-        case .convective_inhibition: return .joulePerKilogram
-        case .shortwave_radiation_clear_sky:
-            return .wattPerSquareMetre
-        case .precipitation_type:
-            return .undefined
-        case .snowfall:
-            return .centimetre
         }
     }
     
@@ -307,13 +223,13 @@ enum BomSurfaceVariable: String, CaseIterable, GenericVariableMixable, BomVariab
         switch self {
         case .surface_temperature:
             fallthrough
-        case .soil_temperature_0_to_10cm:
+        case .soil_temperature_10cm:
             fallthrough
-        case .soil_temperature_10_to_40cm:
+        case .soil_temperature_35cm:
             fallthrough
-        case .soil_temperature_40_to_100cm:
+        case .soil_temperature_100cm:
             fallthrough
-        case .soil_temperature_100_to_200cm:
+        case .soil_temperature_300cm:
             fallthrough
         case .temperature_2m:
             return true
