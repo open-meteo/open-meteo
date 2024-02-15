0c91c8c0334c4037df41b558f6f99bb1461c8d65
patrick-zippenfenig <patrick@zippenfenig.de>
2024-02-15 09:42:44 +0100

    cleanup

--- Sources/SwiftPFor2D/MmapFile.swift
+++ Sources/SwiftPFor2D/MmapFile.swift
@@ -1,23 +1,35 @@
 import Foundation
 
-/// Mmap a all pages for a file
+/// `Mmap` all pages for a file
 public final class MmapFile {
     public let data: UnsafeBufferPointer<UInt8>
     public let file: FileHandle
+    
+    public enum Mode {
+        case readOnly
+        case readWrite
+        
+        /// mmap `prot` attribute
+        fileprivate var prot: Int32 {
+            switch self {
+            case .readOnly:
+                return PROT_READ
+            case .readWrite:
+                return PROT_READ | PROT_WRITE
+            }
+        }
+    }
 
     /// Mmap the entire filehandle
-    public init(fn: FileHandle) throws {
+    public init(fn: FileHandle, mode: Mode = .readOnly) throws {
         let len = try Int(fn.seekToEnd())
-                
-        // for write: guard let mem = mmap(nil, len, PROT_READ | PROT_WRITE, MAP_SHARED, fn, 0),
-        guard let mem = mmap(nil, len, PROT_READ, MAP_SHARED, fn.fileDescriptor, 0), mem != UnsafeMutableRawPointer(bitPattern: -1) else {
+        guard let mem = mmap(nil, len, mode.prot, MAP_SHARED, fn.fileDescriptor, 0), mem != UnsafeMutableRawPointer(bitPattern: -1) else {
             let error = String(cString: strerror(errno))
             throw SwiftPFor2DError.cannotOpenFile(errno: errno, error: error)
         }
         //madvise(mem, len, MADV_SEQUENTIAL)
         let start = mem.assumingMemoryBound(to: UInt8.self)
-        data = UnsafeBufferPointer(start: start, count: len)
-        
+        self.data = UnsafeBufferPointer(start: start, count: len)
         self.file = fn
     }
     
--- Sources/SwiftPFor2D/MmapFileCached.swift
+++ Sources/SwiftPFor2D/MmapFileCached.swift
@@ -3,12 +3,12 @@ import Foundation
 /// Mmap a all pages for a file
 public final class MmapFileCached {
     public let backend: MmapFile
-    public let frontend: MmapMutableFile?
+    public let frontend: MmapFile?
     public let cacheFile: String?
 
     public init(backend: FileHandle, frontend: FileHandle?, cacheFile: String?) throws {
         self.backend = try MmapFile(fn: backend)
-        self.frontend = try frontend.map { try MmapMutableFile(fn: $0) }
+        self.frontend = try frontend.map { try MmapFile(fn: $0, mode: .readWrite) }
         self.cacheFile = cacheFile
     }
     
@@ -37,6 +37,7 @@ public final class MmapFileCached {
 }
 
 extension MmapFileCached: OmFileReaderBackend {
+    /// Populate cache frontend before reading
     public func preRead(offset: Int, count: Int) {
         guard let frontend else {
             return
@@ -45,11 +46,12 @@ extension MmapFileCached: OmFileReaderBackend {
         let blockSize = 128*1024
         let blockStart = offset.floor(to: blockSize)
         let blockEnd = (offset + count).ceil(to: blockSize)
+        let backendData = UnsafeMutableBufferPointer(mutating: backend.data)
+        let frontendData = UnsafeMutableBufferPointer(mutating: frontend.data)
         for block in stride(from: blockStart, to: blockEnd, by: blockSize) {
-            let range = block..<min(block+blockSize, backend.data.count)
-            if frontend.data.allZero(range) {
-                let backendData = UnsafeMutableBufferPointer(mutating: backend.data)
-                frontend.data[range] = backendData[range]
+            let range = block..<min(block+blockSize, backendData.count)
+            if frontendData.allZero(range) {
+                frontendData[range] = backendData[range]
             }
         }
     }
@@ -64,56 +66,12 @@ extension MmapFileCached: OmFileReaderBackend {
     
     public func withUnsafeBytes<ResultType>(_ body: (UnsafeRawBufferPointer) throws -> ResultType) rethrows -> ResultType {
         if let frontend {
-            try frontend.data.withUnsafeBytes(body)
+            try frontend.withUnsafeBytes(body)
         } else {
             try backend.withUnsafeBytes(body)
         }
     }
 }
-
-
-/// Similar to MmapFile, but mutable
-public final class MmapMutableFile {
-    public let data: UnsafeMutableBufferPointer<UInt8>
-    public let file: FileHandle
-
-    /// Mmap the entire filehandle
-    public init(fn: FileHandle) throws {
-        let len = try Int(fn.seekToEnd())
-        guard let mem = mmap(nil, len, PROT_READ | PROT_WRITE, MAP_SHARED, fn.fileDescriptor, 0), mem != UnsafeMutableRawPointer(bitPattern: -1) else {
-            let error = String(cString: strerror(errno))
-            throw SwiftPFor2DError.cannotOpenFile(errno: errno, error: error)
-        }
-        //madvise(mem, len, MADV_SEQUENTIAL)
-        let start = mem.assumingMemoryBound(to: UInt8.self)
-        data = UnsafeMutableBufferPointer(start: start, count: len)
-        self.file = fn
-    }
-    
-    /// Check if the file was deleted on the file system. Linux keep the file alive, as long as some processes have it open.
-    public func wasDeleted() -> Bool {
-        file.wasDeleted()
-    }
-    
-    public func prefetchData(offset: Int, count: Int) {
-        let pageStart = offset.floor(to: 4096)
-        let pageEnd = (offset + count).ceil(to: 4096)
-        let length = pageEnd - pageStart
-        let ret = madvise(UnsafeMutableRawPointer(mutating: data.baseAddress!.advanced(by: pageStart)), length, MADV_WILLNEED)
-        guard ret == 0 else {
-            let error = String(cString: strerror(errno))
-            fatalError("madvice failed! ret=\(ret), errno=\(errno), \(error)")
-        }
-    }
-
-    deinit {
-        let len = data.count * MemoryLayout<UInt8>.size
-        guard munmap(UnsafeMutableRawPointer(mutating: data.baseAddress!), len) == 0 else {
-            fatalError("munmap failed")
-        }
-    }
-}
-
 
 extension UnsafeMutableBufferPointer where Element == UInt8 {
     /// Check if a range contains all zero bytes
