004b8cdeafcc718cf5f1af038b3f1acfb0cfeb99
patrick-zippenfenig <patrick@zippenfenig.de>
2024-07-29 18:23:02 +0200

    fix: cloud base in DMI

--- Sources/App/Dmi/DmiDownloader.swift
+++ Sources/App/Dmi/DmiDownloader.swift
@@ -213,10 +213,10 @@ struct DmiDownload: AsyncCommand {
                             grib2d.array.data.multiplyAdd(multiply: 1/3600, add: 0)
                         case .cloud_top, .cloud_base:
                             // Cloud base and top mark "no clouds" as NaN
-                            // Set it to infinity to work with conversion
+                            // Set it to 0 to work with conversion
                             for i in grib2d.array.data.indices {
                                 if grib2d.array.data[i].isNaN {
-                                    grib2d.array.data[i] = .infinity
+                                    grib2d.array.data[i] = 0
                                 }
                             }
                         default:
