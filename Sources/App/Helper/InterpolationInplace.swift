9216c1762bad8b677d8f1e4f7ac76958ecf21b99
patrick-zippenfenig <patrick@zippenfenig.de>
2024-07-29 23:02:41 +0200

    fix: interpolation NaN check

--- Sources/App/Helper/InterpolationInplace.swift
+++ Sources/App/Helper/InterpolationInplace.swift
@@ -36,7 +36,7 @@ extension Array where Element == Float {
         if self.onlyNaN() {
             return
         }
-        guard self[0 ..< Swift.min(6, time.count)].hasValidData() else {
+        if self[0..<Swift.min(6, time.count)].onlyNaN() && self[Swift.min(6, time.count)..<time.count].hasValidData() {
             fatalError("First 6 timesteps only contains NaN, followed by valid data. This leads to issues in interpolation.")
         }
         
