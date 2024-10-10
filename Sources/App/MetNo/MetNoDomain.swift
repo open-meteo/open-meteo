8c7476ed1308b62b895fa0c52fddde25f6509d4f
patrick-zippenfenig <patrick@zippenfenig.de>
2024-10-10 08:56:41 +0200

    fix: metno again

--- Sources/App/MetNo/MetNoDomain.swift
+++ Sources/App/MetNo/MetNoDomain.swift
@@ -188,7 +188,7 @@ enum MetNoVariable: String, CaseIterable, GenericVariable, GenericVariableMixabl
     
     var isAccumulatedSinceModelStart: Bool {
         switch self {
-        case .precipitation: return true
+        case .shortwave_radiation: return true
         default: return false
         }
     }
