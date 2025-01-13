8afad3fed86b22c3bb6b9b38b819c7ec6c390ed1
patrick-zippenfenig <patrick@zippenfenig.de>
2025-01-13 10:10:57 +0100

    test backtrace crash handler

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -3,9 +3,15 @@ import Vapor
 struct WebsiteController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         routes.get("robots.txt", use: robotsTxtHandler)
+        routes.get("test_backtrace", use: testBacktrace)
     }
     
     func robotsTxtHandler(_ req: Request) -> String {
         return "User-agent: *\nDisallow: /"
+    }
+    
+    func testBacktrace(_ req: Request) -> String {
+        let array = [0,1,2]
+        return String(array[10])
     }
 }
