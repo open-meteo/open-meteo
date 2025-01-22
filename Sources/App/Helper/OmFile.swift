2e4ba3f80108ffbd7307843777f75da782c1baa0
patrick-zippenfenig <patrick@zippenfenig.de>
2025-01-22 14:56:00 +0100

    remove file writer for old file format

--- Sources/App/Helper/OmFile.swift
+++ Sources/App/Helper/OmFile.swift
@@ -192,131 +192,23 @@ struct OmFileSplitter {
     /**
      Write new data to the archived storage and combine it with existint data.
      Updates are done in chunks to keep memory size low. Otherwise ICON update would take 4+ GB memory for just this function.
+     
+     TODO: should use Array3DFastTime
      */
     func updateFromTimeOriented(variable: String, array2d: Array2DFastTime, time: TimerangeDt, scalefactor: Float, compression: CompressionType = .pfor_delta2d_int16) throws {
-        
         precondition(array2d.nTime == time.count)
+        precondition(array2d.nLocations == nx * ny)
         
         // Process at most 8 MB at once
-        try updateFromTimeOrientedStreaming(variable: variable, time: time, scalefactor: scalefactor, compression: compression, onlyGeneratePreviousDays: false) { d0offset in
-            
-            let locationRange = d0offset ..< min(d0offset+nLocationsPerChunk, nLocations)
+        try updateFromTimeOrientedStreaming3D(variable: variable, time: time, scalefactor: scalefactor, compression: compression, onlyGeneratePreviousDays: false) { (yRange, xRange, memberRange) in
+            guard yRange.count == 1 || xRange.count == nx else {
+                fatalError("chunk dimensions need to be either parts of X or a mutliple or X")
+            }
+            let locationRange = (Int(yRange.lowerBound) * nx + Int(xRange.lowerBound)) ..< (Int(yRange.upperBound - 1) * nx + Int(xRange.lowerBound))
             let dataRange = locationRange.multiply(array2d.nTime)
             return array2d.data[dataRange]
         }
     }
-    
-    /**
-     Write new data to archived storage and combine it with existing data.
-     `supplyChunk` should provide data for a couple of thousands locations at once. Upates are done streamlingly to low memory usage
-     */
-    func updateFromTimeOrientedStreaming(variable: String, time: TimerangeDt, scalefactor: Float, compression: CompressionType = .pfor_delta2d_int16, onlyGeneratePreviousDays: Bool, supplyChunk: (_ dim0Offset: Int) throws -> ArraySlice<Float>) throws {
-        
-        let indexTime = time.toIndexTime()
-        let indextimeChunked = indexTime.divideRoundedUp(divisor: nTimePerFile)
-        
-        /// Previous days of forecast to keep. Max 7 past days
-        /// `0..<1` if previous days are not generated
-        /// `1..<n` for all previous days
-        let previousDaysRange: Range<Int> = onlyGeneratePreviousDays ? (1..<max(1, min(8, time.range.count / 86400))) : (0..<1)
-        if previousDaysRange.isEmpty {
-            return
-        }
-        
-        struct WriterPerStep {
-            let read: OmFileReader<MmapFile>?
-            let write: OmFileWriterState<FileHandle>
-            let offsets: (file: CountableRange<Int>, array: CountableRange<Int>)
-            let fileName: String
-            let skip: Int
-        }
-        
-        // open all files for all timeranges and write a header
-        let writers: [WriterPerStep] = try indextimeChunked.flatMap { timeChunk -> [WriterPerStep] in
-            let fileTime = timeChunk * nTimePerFile ..< (timeChunk+1) * nTimePerFile
-            guard let offsets = indexTime.intersect(fileTime: fileTime) else {
-                return []
-            }
-            
-            return try previousDaysRange.map { previousDay -> WriterPerStep in
-                let skip =  previousDay * 86400 / time.dtSeconds
-                let readFile = OmFileManagerReadable.domainChunk(domain: domain, variable: variable, type: .chunk, chunk: timeChunk, ensembleMember: 0, previousDay: previousDay)
-                try readFile.createDirectory()
-                let tempFile = readFile.getFilePath() + "~"
-                // Another process might be updating this file right now. E.g. Second flush of GFS ensemble
-                FileManager.default.waitIfFileWasRecentlyModified(at: tempFile)
-                try FileManager.default.removeItemIfExists(at: tempFile)
-                let fn = try FileHandle.createNewFile(file: tempFile)
-                
-                let omRead = try readFile.openRead()
-                try omRead?.willNeed()
-
-                let omWrite = try OmFileWriterState<FileHandle>(fn: fn, dim0: nLocations, dim1: nTimePerFile, chunk0: chunknLocations, chunk1: nTimePerFile, compression: compression, scalefactor: scalefactor, fsync: true)
-
-                try omWrite.writeHeader()
-                
-                return WriterPerStep(read: omRead, write: omWrite, offsets: offsets, fileName: readFile.getFilePath(), skip: skip)
-            }
-        }
-        
-        let nIndexTime = indexTime.count
-        var fileData = [Float]()
-        
-        // loop chunks of locations
-        var dim0Offset = 0
-        while dim0Offset < nLocations {
-            let data = try supplyChunk(dim0Offset)
-            let nLocInChunk = data.count / nIndexTime
-            //print("nLocaInChunk \(nLocInChunk) dim0Offset \(dim0Offset) total \(nLocations) \(Float(dim0Offset)/Float(nLocations))%")
-            if fileData.count < nLocInChunk * nTimePerFile {
-                fileData = [Float](repeating: .nan, count: nLocInChunk * nTimePerFile)
-            }
-            let locationRange = dim0Offset ..< min(dim0Offset+nLocInChunk, nLocations)
-            
-            for writer in writers {
-                // Read existing data for a chunk of locations
-                if let omRead = writer.read, omRead.dim0 == nLocations, omRead.dim1 == nTimePerFile {
-                    try omRead.read(into: &fileData, arrayDim1Range: 0..<locationRange.count * nTimePerFile, arrayDim1Length: nTimePerFile, dim0Slow: locationRange, dim1: 0..<nTimePerFile)
-                } else {
-                    /// If the old file does not exist, just make sure it is filled with NaNs
-                    for i in fileData.indices {
-                        fileData[i] = .nan
-                    }
-                }
-                
-                // write "new" data into existing data
-                for l in 0..<locationRange.count {
-                    for (tFile,tArray) in zip(writer.offsets.file, writer.offsets.array) {
-                        if tArray < writer.skip {
-                            continue
-                        }
-                        /*if nIndexTime - tArray <= skipLast {
-                            continue
-                        }*/
-                        if data[data.startIndex + l * nIndexTime + tArray].isNaN {
-                            continue
-                        }
-                        fileData[nTimePerFile * l + tFile] = data[data.startIndex + l * nIndexTime + tArray]
-                    }
-                }
-                
-                // Write data
-                try writer.write.write(fileData[0..<locationRange.count * nTimePerFile])
-            }
-            
-            dim0Offset += nLocInChunk
-        }
-        
-        /// Write end of file and move it in position
-        for writer in writers {
-            try writer.write.writeTail()
-            try writer.write.fn.close()
-            
-            // Overwrite existing file, with newly created
-            try FileManager.default.moveFileOverwrite(from: "\(writer.fileName)~", to: writer.fileName)
-        }
-    }
-    
     
     /**
      Write new data to archived storage and combine it with existing data.
