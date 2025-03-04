e7278d13840f5f76e912ddc631a56fe71ce2b94f
patrick-zippenfenig <patrick@zippenfenig.de>
2025-03-04 18:06:28 +0100

    refactor TransferAmountTrackerActor

--- Sources/App/Helper/Download/TransferAmountTrackerActor.swift
+++ Sources/App/Helper/Download/TransferAmountTrackerActor.swift
@@ -8,54 +8,29 @@ extension AsyncSequence where Element == ByteBuffer {
 }
 
 final actor TransferAmountTrackerActor {
-    var transfered = 0
-    var transferedLastPrint = 0
-    let printDelta: Double = 20
-    let startTime = Date()
-    var lastPrint = Date()
-    let logger: Logger
-    let totalSize: Int?
-    let name: String
+    private let tracker: TransferAmountTracker
+    
+    var transfered: Int {
+        tracker.transfered
+    }
     
     public init(logger: Logger, totalSize: Int?, name: String = "Transfer") {
-        self.logger = logger
-        self.totalSize = totalSize
-        self.name = name
+        self.tracker = TransferAmountTracker(logger: logger, totalSize: totalSize, name: name)
     }
     
     /// Print status from time to time
     func set(_ bytes: Int) {
-        transfered = bytes
-        let deltaT = Date().timeIntervalSince(lastPrint)
-        if deltaT > printDelta {
-            let timeElapsed = Date().timeIntervalSince(startTime).asSecondsPrettyPrint
-            let rate = (transfered - transferedLastPrint) / Int(deltaT)
-            if let totalSize {
-                let percent = Int(round(Float(transfered) / Float(totalSize) * 100))
-                let remainingTime = Double(totalSize - transfered) / (Double(transfered - transferedLastPrint) / deltaT)
-                logger.info("\(name) \(percent)% \(transfered.bytesHumanReadable) / \(totalSize.bytesHumanReadable) in \(timeElapsed), \(rate.bytesHumanReadable)/s remaining \(remainingTime.asSecondsPrettyPrint)")
-            } else {
-                logger.info("\(name) \(transfered.bytesHumanReadable) in \(timeElapsed), \(rate.bytesHumanReadable)/s")
-            }
-
-            lastPrint = Date()
-            transferedLastPrint = transfered
-        }
+        tracker.set(bytes)
     }
     
     /// Print status from time to time
     func add(_ bytes: Int) {
-        set(bytes + transfered)
+        tracker.add(bytes)
     }
     
     /// Print end statistics
     func finish() {
-        guard transfered > 0 else {
-            return
-        }
-        let timeElapsed = Date().timeIntervalSince(startTime)
-        let rate = Int(Double(transfered) / timeElapsed)
-        logger.info("\(name) completed \(transfered.bytesHumanReadable) in \(timeElapsed.asSecondsPrettyPrint). Average speed \(rate.bytesHumanReadable)/s")
+        tracker.finish()
     }
 }
 
