b344b310908d1e1d6256c7222ad023d41a7bd6c7
patrick-zippenfenig <patrick@zippenfenig.de>
2025-04-16 12:04:41 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
         .package(url: "https://github.com/patrick-zippenfenig/SwiftNetCDF.git", from: "1.1.2"),
         .package(url: "https://github.com/patrick-zippenfenig/SwiftTimeZoneLookup.git", from: "1.0.7"),
         .package(url: "https://github.com/patrick-zippenfenig/SwiftEccodes.git", from: "1.0.1"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.68.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.68.0")
         //.package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.59.1")
     ] + (enableParquet ? [
         .package(url: "https://github.com/patrick-zippenfenig/SwiftArrowParquet.git", from: "1.0.0")
@@ -64,7 +64,7 @@ let package = Package(
                 .product(name: "SwiftArrowParquet", package: "SwiftArrowParquet")
             ] : []),
             cSettings: cFlags,
-            swiftSettings: swiftFlags + (enableParquet ? [.define("ENABLE_PARQUET")] : []),
+            swiftSettings: swiftFlags + (enableParquet ? [.define("ENABLE_PARQUET")] : [])
             //plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
         ),
         .systemLibrary(
