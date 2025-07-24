935527cb9f8167c5847c30e788480f49d2312a93
patrick-zippenfenig <patrick@zippenfenig.de>
2025-07-24 16:22:19 +0200

    add valid_times to per run meta.json

--- Sources/App/Helper/Writer/GenericVariableHandle.swift
+++ Sources/App/Helper/Writer/GenericVariableHandle.swift
@@ -210,7 +210,8 @@ struct GenericVariableHandle: Sendable {
             try FileManager.default.moveFileOverwrite(from: fileTemp, to: filePath)
             progress.finish()
         }
-        try FullRunMetaJson.write(domain: domain, run: run)
+        let validTimes = handles.flatMap({$0.time.map({$0})}).uniqued().sorted()
+        try FullRunMetaJson.write(domain: domain, run: run, validTimes: validTimes)
     }
 
     private static func convertConcurrent(logger: Logger, domain: GenericDomain, createNetcdf: Bool, run: Timestamp?, handles: [Self], onlyGeneratePreviousDays: Bool, concurrent: Int, compression: OmCompressionType) async throws {
@@ -358,6 +359,7 @@ fileprivate struct FullRunMetaJson: Encodable {
     let reference_time: Date
     let created_at: Date
     let variables: [String]
+    let valid_times: [String]
 
     /// Data temporal resolution in seconds. E.g. 3600 for 1-hourly data
     let temporal_resolution_seconds: Int
@@ -365,7 +367,7 @@ fileprivate struct FullRunMetaJson: Encodable {
     // valid_times? Params like precipitation do not have the first step. Some MeteoFrance variables are also missing steps...
     
     /// Use directory listing to get all variables. Model or pressure levels might be downloaded at a different time
-    public init?(domain: GenericDomain, run: Timestamp) throws {
+    public init?(domain: GenericDomain, run: Timestamp, validTimes: [Timestamp]) throws {
         let path = "\(domain.dataRunDirectory!)\(run.format_directoriesYYYYMMddhhmm)/"
         guard FileManager.default.fileExists(atPath: path) else {
             print("Directory does not exist to generate FullRunMetaJson: \(path)")
@@ -376,10 +378,11 @@ fileprivate struct FullRunMetaJson: Encodable {
         self.reference_time = run.toDate()
         self.created_at = Date()
         self.temporal_resolution_seconds = domain.dtSeconds
+        self.valid_times = validTimes.map(\.iso8601_YYYY_MM_dd_HH_mmZ)
     }
     
-    static func write(domain: GenericDomain, run: Timestamp) throws {
-        guard let meta = try FullRunMetaJson(domain: domain, run: run) else {
+    static func write(domain: GenericDomain, run: Timestamp, validTimes: [Timestamp]) throws {
+        guard let meta = try FullRunMetaJson(domain: domain, run: run, validTimes: validTimes) else {
             return
         }
         let path = "\(domain.dataRunDirectory!)\(run.format_directoriesYYYYMMddhhmm)/meta.json"
