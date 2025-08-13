65c915fa799116f4421e7790548b23acf1bb3fe0
patrick-zippenfenig <patrick@zippenfenig.de>
2025-08-13 14:54:53 +0200

    bump om file format to proactively use v3 files

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         .package(url: "https://github.com/google/flatbuffers.git", from: "25.2.10"),
         .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
         .package(url: "https://github.com/open-meteo/sdk.git", from: "1.20.0"),
-        .package(url: "https://github.com/open-meteo/om-file-format.git", revision: "01b48bf682c50e736749569cc2320298a08e80a9"), // Because unsafe C flags are set, tagged releases cannot be used
+        .package(url: "https://github.com/open-meteo/om-file-format.git", revision: "fcda3d94e7882cc462b2be3e7e5a360fb47956ba"), // Because unsafe C flags are set, tagged releases cannot be used
         // .package(path: "../openmeteo-sdk-fork"),  // local forked version
         //.package(url: "https://github.com/open-meteo/sdk.git", branch: "add_ecmwf_long_window"),
         .package(url: "https://github.com/patrick-zippenfenig/curl-swift.git", from: "1.0.1"),
