b39a257d1c95b55a67649fabccc748e52ef9a146
Patrick Zippenfenig <patrick@zippenfenig.de>
2025-10-20 14:22:00 +0200

    feat: Add Eumetsat MTG solar radiation data via JMA (#1536)

--- Sources/App/JaxaHimawari/JaxaHimawariDomain.swift
+++ Sources/App/JaxaHimawari/JaxaHimawariDomain.swift
@@ -1,10 +1,17 @@
 enum JaxaHimawariDomain: String, GenericDomain, CaseIterable {
     case himawari_10min
+    /// Extented domain area to 70E
+    case himawari_70e_10min
+    case mtg_fci_10min
 
     var domainRegistry: DomainRegistry {
         switch self {
         case .himawari_10min:
             return .jma_jaxa_himawari_10min
+        case .himawari_70e_10min:
+            return .jma_jaxa_himawari_70e_10min
+        case .mtg_fci_10min:
+            return .jma_jaxa_mtg_fci_10min
         }
     }
 
@@ -13,7 +20,14 @@ enum JaxaHimawariDomain: String, GenericDomain, CaseIterable {
     }
 
     var hasYearlyFiles: Bool {
-        return true
+        switch self {
+        case .himawari_10min:
+            return true
+        case .himawari_70e_10min:
+            return false
+        case .mtg_fci_10min:
+            return false
+        }
     }
 
     var masterTimeRange: Range<Timestamp>? {
@@ -22,13 +36,21 @@ enum JaxaHimawariDomain: String, GenericDomain, CaseIterable {
 
     var dtSeconds: Int {
         switch self {
-        case .himawari_10min:
+        case .himawari_10min, .mtg_fci_10min, .himawari_70e_10min:
             return 600
         }
     }
 
     var grid: any Gridable {
-        return RegularGrid(nx: 2401, ny: 2401, latMin: -60, lonMin: 80, dx: 0.05, dy: 0.05)
+        switch self {
+        case .himawari_10min:
+            return RegularGrid(nx: 2401, ny: 2401, latMin: -60, lonMin: 80, dx: 0.05, dy: 0.05)
+        case .himawari_70e_10min:
+            return RegularGrid(nx: 2801, ny: 2401, latMin: -60, lonMin: 70, dx: 0.05, dy: 0.05)
+        case .mtg_fci_10min:
+            return RegularGrid(nx: 2801, ny: 2401, latMin: -60, lonMin: -70, dx: 0.05, dy: 0.05)
+        }
+        
     }
 
     var updateIntervalSeconds: Int {
@@ -37,7 +59,7 @@ enum JaxaHimawariDomain: String, GenericDomain, CaseIterable {
 
     var omFileLength: Int {
         switch self {
-        case .himawari_10min:
+        case .himawari_10min, .mtg_fci_10min, .himawari_70e_10min:
             return 6 * 24 * 2
         }
     }
