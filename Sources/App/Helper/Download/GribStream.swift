0b6bb74229acb0ed8d20196c7a9c03e232065079
patrick-zippenfenig <patrick@zippenfenig.de>
2025-11-14 16:39:00 +0100

    restore grib stream

--- Sources/App/Helper/Download/GribStream.swift
+++ Sources/App/Helper/Download/GribStream.swift
@@ -3,58 +3,88 @@ import NIO
 import CBz2lib
 @preconcurrency import SwiftEccodes
 import Logging
-import AsyncAlgorithms
 
-extension AsyncSequence where Self: Sendable, Element == ByteBuffer {
-    /// Decode incoming data to GRIB messages. Processing takes place in detached task
-    func decodeGrib() -> AsyncThrowingChannel<GribMessage, Error> {
-        let channel = AsyncThrowingChannel<GribMessage, Error>()
-        _ = Task {
-            var iterator = self.makeAsyncIterator()
-            var buffer = ByteBuffer()
-            do {
-                while true {
-                    // repeat until GRIB header is found
-                    guard let seek = buffer.withUnsafeReadableBytes(GribAsyncStreamHelper.seekGrib) else {
-                        guard let input = try await iterator.next() else {
-                            break // end of stream
-                        }
-                        guard buffer.readableBytes < 64 * 1024 else {
-                            throw GribAsyncStreamError.didNotFindGibHeader
-                        }
-                        buffer.writeImmutableBuffer(input)
+
+extension AsyncSequence where Element == ByteBuffer {
+    /// Decode incoming data to GRIB messages
+    func decodeGrib() -> GribAsyncStream<Self> {
+        return GribAsyncStream(sequence: self)
+    }
+}
+
+/*
+ Decode incoming binary stream to GRIB messages
+ */
+struct GribAsyncStream<T: AsyncSequence>: AsyncSequence where T.Element == ByteBuffer {
+    public typealias Element = AsyncIterator.Element
+
+    let sequence: T
+
+    public final class AsyncIterator: AsyncIteratorProtocol {
+        private var iterator: T.AsyncIterator
+
+        /// Collect enough bytes to decompress a single message
+        private var buffer: ByteBuffer
+
+        /// Buffer mutliple messages to only return one at a time
+        private var messages: [GribMessage]?
+
+        fileprivate init(iterator: T.AsyncIterator) {
+            self.iterator = iterator
+            self.buffer = ByteBuffer()
+            buffer.reserveCapacity(minimumWritableBytes: 4096)
+        }
+
+        public func next() async throws -> GribMessage? {
+            if let next = messages?.popLast() {
+                return next
+            }
+
+            while true {
+                // repeat until GRIB header is found
+                guard let seek = buffer.withUnsafeReadableBytes(GribAsyncStreamHelper.seekGrib) else {
+                    guard let input = try await self.iterator.next() else {
+                        return nil
+                    }
+                    guard buffer.readableBytes < 64 * 1024 else {
+                        throw GribAsyncStreamError.didNotFindGibHeader
+                    }
+                    buffer.writeImmutableBuffer(input)
+                    continue
+                }
+
+                // Repeat until enough data is available
+                while buffer.readableBytes < seek.offset + seek.length {
+                    guard let input = try await self.iterator.next() else {
+                        // If EOF is reached before message length, still try to decode it with eccodes
                         continue
                     }
-
-                    // Repeat until enough data is available
-                    while buffer.readableBytes < seek.offset + seek.length {
-                        guard let input = try await iterator.next() else {
-                            // If EOF is reached before message length, still try to decode it with eccodes
-                            continue
-                        }
-                        buffer.writeImmutableBuffer(input)
-                    }
-
-                    // Decode message with eccodes
-                    let messages = try buffer.readWithUnsafeReadableBytes({
-                        let range = seek.offset ..< Swift.min(seek.offset + seek.length, $0.count)
-                        let memory = UnsafeRawBufferPointer(rebasing: $0[range])
-                        let messages = try SwiftEccodes.getMessages(memory: memory, multiSupport: true)
-                        return (seek.offset + seek.length, messages)
-                    })
-
-                    buffer.discardReadBytes()
-                    for message in messages {
-                        await channel.send(message)
-                    }
+                    buffer.writeImmutableBuffer(input)
                 }
-                channel.finish()
-            } catch {
-                channel.fail(error)
+
+                // Deocode message with eccodes
+                messages = try buffer.readWithUnsafeReadableBytes({
+                    let range = seek.offset ..< Swift.min(seek.offset + seek.length, $0.count)
+                    let memory = UnsafeRawBufferPointer(rebasing: $0[range])
+                    let messages = try SwiftEccodes.getMessages(memory: memory, multiSupport: true)
+                    return (seek.offset + seek.length, messages)
+                })
+
+                buffer.discardReadBytes()
+                if let next = messages?.popLast() {
+                    return next
+                }
             }
         }
-        return channel
     }
+
+    public func makeAsyncIterator() -> AsyncIterator {
+        AsyncIterator(iterator: sequence.makeAsyncIterator())
+    }
+}
+
+extension GribAsyncStream: Sendable where T: Sendable {
+    
 }
 
 enum GribAsyncStreamHelper {
