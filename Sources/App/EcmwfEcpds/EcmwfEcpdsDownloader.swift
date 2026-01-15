49432bdc00259cf4d96a76baaf630a684bf998dc
Patrick Zippenfenig <patrick@zippenfenig.de>
2026-01-15 14:56:36 +0100

    feat: Downloader for IFS HRES past runs via ECMWF MARS API (#1675)

--- Sources/App/EcmwfEcpds/EcmwfEcpdsDownloader.swift
+++ Sources/App/EcmwfEcpds/EcmwfEcpdsDownloader.swift
@@ -31,6 +31,12 @@ struct DownloadEcmwfEcpdsCommand: AsyncCommand {
 
         @Flag(name: "upload-s3-only-probabilities", help: "Only upload probabilities files to S3")
         var uploadS3OnlyProbabilities: Bool
+        
+        @Option(name: "key", short: "k", help: "ECMWF key")
+        var key: String?
+
+        @Option(name: "email", help: "Email for the ECMWF API service")
+        var email: String?
     }
 
     var help: String {
@@ -49,18 +55,19 @@ struct DownloadEcmwfEcpdsCommand: AsyncCommand {
         let logger = context.application.logger
 
         let nConcurrent = signature.concurrent ?? 2
+
+        if let timeinterval = signature.timeinterval {
+            guard let email = signature.email, let key = signature.key else {
+                fatalError("Email and key required")
+            }
+            let runs = try Timestamp.parseRange(yyyymmdd: timeinterval).toRange(dt: 86400).with(dtSeconds: 86400 / 4)
+            try await downloadMars(application: context.application, domain: domain, runs: runs, concurrent: nConcurrent, key: key, email: email, uploadS3Bucket: signature.uploadS3Bucket)
+            return
+        }
         guard let server = signature.server else {
             fatalError("Parameter server is required")
         }
 
-        if let timeinterval = signature.timeinterval {
-            for run in try Timestamp.parseRange(yyyymmdd: timeinterval).toRange(dt: 86400).with(dtSeconds: 86400 / 4) {
-                logger.info("Downloading domain ECMWF run '\(run.iso8601_YYYY_MM_dd_HH_mm)'")
-                let handles = try await downloadEcmwf(application: context.application, domain: domain, server: server, run: run, concurrent: nConcurrent, maxForecastHour: signature.maxForecastHour, uploadS3Bucket: nil)
-                try await GenericVariableHandle.convert(logger: logger, domain: domain, createNetcdf: signature.createNetcdf, run: run, handles: handles, concurrent: nConcurrent, writeUpdateJson: false, uploadS3Bucket: nil, uploadS3OnlyProbabilities: false, generateTimeSeries: !signature.skipTimeseries)
-            }
-            return
-        }
         logger.info("Downloading domain ECMWF run '\(run.iso8601_YYYY_MM_dd_HH_mm)'")
 
         try await downloadEcmwfElevation(application: context.application, domain: domain, run: run)
@@ -131,6 +138,163 @@ struct DownloadEcmwfEcpdsCommand: AsyncCommand {
         try domain.surfaceElevationFileOm.createDirectory()
         try elevation.writeOmFile2D(file: domain.surfaceElevationFileOm.getFilePath(), grid: domain.grid)*/
     }
+    
+    /// Download multiple runs from ECMWF MARS archives, convert them and upload to S3
+    func downloadMars(application: Application, domain: EcmwfEcpdsDomain, runs: TimerangeDt, concurrent: Int, key: String, email: String, uploadS3Bucket: String?) async throws {
+        let logger = application.logger
+        let dtSeconds = domain.dtSeconds
+        
+        struct EcmwfQuery: Encodable {
+            let `class` = "od"
+            /// iso string `2016-03-18`
+            let date: String
+            let expver = 1
+            let levtype = "sfc"
+            let param: String
+            /// Use forecast hours 1...12. Skip hour 0, as the model is instable at hour 0
+            let step: String
+            let stream: String
+            /// init time "00:00:00"
+            let time: String
+            let type: String
+        }
+        
+        let client = application.makeNewHttpClient(redirectConfiguration: .disallow)
+        let curl = Curl(logger: logger, client: client, deadLineHours: 999999)
+        
+        let sideRunSteps = ["0/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30/31/32/33/34/35/36/37/38/39/40/41/42/43/44/45/46/47/48/49/50/51/52/53/54/55/56/57/58/59/60/61/62/63/64/65/66/67/68/69/70/71/72/73/74/75/76/77/78/79/80/81/82/83/84/85/86/87/88/89/90/93/96/99/102/105/108/111/114/117/120/123/126/129/132/135/138/141/144"]
+        /// Split 0z/12z runs into 2 requests, because MARS transfers are limited to 75 GB
+        let fullRunSteps = [sideRunSteps[0], "150/156/162/168/174/180/186/192/198/204/210/216/222/228/234/240/246/252/258/264/270/276/282/288/294/300/306/312/318/324/330/336/342/348/354/360"]
+        
+        /// Run process task in the background
+        var processTask: Task<(), any Error>? = nil
+        
+        for run in runs {
+            logger.info("Downloading run \(run.iso8601_YYYY_MM_dd_HH_mm)")
+            
+            let writer = OmSpatialMultistepWriter(domain: domain, run: run, storeOnDisk: false, realm: nil)
+            let deaverager = GribDeaverager()
+            let stepsArray = run.hour % 12 == 0 ? fullRunSteps : sideRunSteps
+            for steps in stepsArray {
+                // 20.3 = visibility
+                // 145.151 = Sea surface height
+                // 98.174 = Sea ice thickness
+                // 228051 = litota1
+                // 228057 = litota3 + litota6 -> disabled because MARS query fails with wrong number of fields. Need to use a different timerange
+                let query = EcmwfQuery(
+                    date: run.iso8601_YYYY_MM_dd,
+                    param: "100u/100v/10fg/10u/10v/200u/200v/2d/2t/cp/fal/fdir/fsr/hcc/kx/lcc/mcc/mn2t/msl/mucape/mucin/mx2t/pev/ptype/ro/rsn/sd/sf/skt/ssrd/stl1/stl2/stl3/stl4/swvl1/swvl2/swvl3/swvl4/tcc/tcwv/tp/20.3/blh/98.174/ocu/ocv/145.151",
+                    step: steps,
+                    stream: run.hour % 12 == 0 ? "oper" : "scda",
+                    time: "\(run.hh)00",
+                    type: "fc"
+                )
+                
+                let job = try await curl.startEcmwfApiJob(query: query, email: email, apikey: key)
+                let gribUrl = try await curl.waitForEcmwfJob(job: job, email: email, apikey: key)
+                
+                let inMemory = VariablePerMemberStorage<EcmwfEcdpsIfsVariable>()
+                /// Single GRIB files contains multiple time-steps in mixed order
+                let inMemoryAccumulated = VariablePerMemberStorage<EcmwfEcdpsIfsVariable>()
+                
+                // Wait for the previous process task to finish
+                try await processTask?.value
+                processTask = Task {
+                    try await curl.getGribStream(url: gribUrl, bzip2Decode: false, nConcurrent: max(2, concurrent)).foreachConcurrent(nConcurrent: concurrent) { message in
+                        guard let shortName = message.get(attribute: "shortName"),
+                              let unit = message.get(attribute: "units"),
+                              let stepRange = message.get(attribute: "stepRange"),
+                              let stepType = message.get(attribute: "stepType") else {
+                            fatalError("could not get step range or type")
+                        }
+                        let timestamp = try message.getValidTimestamp()
+                        let hour = (timestamp.timeIntervalSince1970 - run.timeIntervalSince1970) / 3600
+                        if shortName == "lsm" {
+                            return
+                        }
+                        //let levelhPa = message.get(attribute: "level").flatMap(Int.init)!
+                        let member = message.get(attribute: "perturbationNumber").flatMap(Int.init) ?? 0
+                        
+                        guard let variable = EcmwfEcdpsIfsVariable.allCases.first(where: {$0.gribCode.split(separator: ",").contains(where: { $0 == shortName})}) else {
+                            logger.warning("Could not map variable \(shortName)")
+                            return
+                        }
+                        
+                        //print(message.get(attribute: "packingType"), message.get(attribute: "bitsPerValue"), message.get(attribute: "binaryScaleFactor"))
+                        let isMax = [EcmwfEcdpsIfsVariable.wind_gusts_10m, .temperature_2m_max, .temperature_2m_min].contains(variable)
+                        let isAccumulated = [EcmwfEcdpsIfsVariable.shortwave_radiation, .direct_radiation, .precipitation, .runoff, .snowfall_water_equivalent, .showers].contains(variable)
+                        /// Gusts in hour 0 only contain `0` values. The attributes for stepType and stepRange are not correctly set.
+                        if (isAccumulated || isMax) && hour == 0 {
+                            return
+                        }
+                        
+                        // logger.info("Processing \(variable)")
+                        var grib2d = GribArray2D(nx: domain.grid.nx, ny: domain.grid.ny)
+                        try grib2d.load(message: message)
+                        
+                        // Scaling before compression with scalefactor
+                        if let fma = variable.multiplyAdd(dtSeconds: dtSeconds) {
+                            grib2d.array.data.multiplyAdd(multiply: fma.multiply, add: fma.add)
+                        }
+                        
+                        // Deaccumulate precipitation
+                        if isAccumulated {
+                            // Collect all accumulated variables and process them as soon as they are in sequential order
+                            await inMemoryAccumulated.set(variable: variable, timestamp: timestamp, member: member, data: grib2d.array)
+                            while true {
+                                let lastStep = await deaverager.lastStep(variable, member) ?? 0
+                                let step = lastStep + (lastStep >= 144 ? 6 : lastStep >= 90 ? 3 : 1)
+                                let time = run.add(hours: step)
+                                guard var data = await inMemoryAccumulated.remove(variable: variable, timestamp: time, member: member) else {
+                                    break
+                                }
+                                guard await deaverager.deaccumulateIfRequired(variable: variable, member: member, stepType: "accum", stepRange: "0-\(step)", array2d: &data) else {
+                                    continue
+                                }
+                                let count = await inMemoryAccumulated.data.count
+                                logger.info("Writing accumulated variable \(variable) member \(member) unit=\(unit) timestamp \(time.format_YYYYMMddHH) backlog \(count)")
+                                try await writer.write(time: time, member: member, variable: variable, data: data.data)
+                            }
+                            return
+                        }
+                        
+                        let writer = try await writer.getWriter(time: timestamp)
+                        
+                        // Snow depth retrieved as water equivalent. Use snow density to calculate the actual snow depth.
+                        if [EcmwfEcdpsIfsVariable.snow_density, .snow_depth].contains(variable) {
+                            await inMemory.set(variable: variable, timestamp: timestamp, member: member, data: grib2d.array)
+                            try await inMemory.calculateSnowDepth(density: .snow_density, waterEquivalent: .snow_depth, outVariable: EcmwfEcdpsIfsVariable.snow_depth, writer: writer)
+                            if variable == .snow_depth {
+                                return
+                            }
+                        }
+                        
+                        logger.info("Processing \(variable) member=\(member) unit=\(unit) stepType=\(stepType) stepRange=\(stepRange) timestep=\(timestamp.format_YYYYMMddHH)")
+                        try await writer.write(member: member, variable: variable, data: grib2d.array.data)
+                    }
+                    /// Delete job from ECMWF MARS queue to free up resources
+                    try await curl.cleanupEcmwfApiJob(job: job, email: email, apikey: key)
+                    
+                    if stepsArray.last == steps {
+                        /// Convert to time-series and upload to AWS
+                        let handles = try await writer.finalise(completed: true, validTimes: nil, uploadS3Bucket: nil)
+                        try await GenericVariableHandle.convert(logger: logger, domain: domain, createNetcdf: false, run: run, handles: handles, concurrent: concurrent, writeUpdateJson: false, uploadS3Bucket: uploadS3Bucket, uploadS3OnlyProbabilities: false, generateTimeSeries: false)
+
+                        if let directory = OpenMeteo.dataRunDirectory, uploadS3Bucket != nil {
+                            // Delete run directory after S3 upload
+                            let model = domain.domainRegistry.rawValue
+                            let timeFormatted = run.format_directoriesYYYYMMddhhmm
+                            let runDir = "\(directory)\(model)/\(timeFormatted)/"
+                            logger.info("Deleting local run directory: \(runDir)")
+                            try FileManager.default.removeItem(atPath: runDir)
+                        }
+                    }
+                }
+            }
+        }
+        try await processTask?.value
+        try await client.shutdown()
+    }
 
     /// Download ECMWF ifs open data
     func downloadEcmwf(application: Application, domain: EcmwfEcpdsDomain, server: String, run: Timestamp, concurrent: Int, maxForecastHour: Int?, uploadS3Bucket: String?) async throws -> [GenericVariableHandle] {
@@ -162,7 +326,6 @@ struct DownloadEcmwfEcpdsCommand: AsyncCommand {
             /// Delta time seconds considering irregular timesteps
             let dtSeconds = previousHour == 0 ? domain.dtSeconds : ((hour - previousHour) * 3600)
             
-            let writerProbabilities = domain.countEnsembleMember > 1 ? OmSpatialTimestepWriter(domain: domain, run: run, time: timestamp, storeOnDisk: true, realm: nil) : nil
             let writer = OmSpatialTimestepWriter(domain: domain, run: run, time: timestamp, storeOnDisk: storeOnDisk, realm: nil)
 
             let inMemory = VariablePerMemberStorage<EcmwfEcdpsIfsVariable>()
@@ -199,7 +362,11 @@ struct DownloadEcmwfEcpdsCommand: AsyncCommand {
                 // logger.info("Processing \(variable)")
                 var grib2d = GribArray2D(nx: domain.grid.nx, ny: domain.grid.ny)
                 try grib2d.load(message: message)
-                grib2d.array.flipLatitude()
+                
+                // Scaling before compression with scalefactor
+                if let fma = variable.multiplyAdd(dtSeconds: dtSeconds) {
+                    grib2d.array.data.multiplyAdd(multiply: fma.multiply, add: fma.add)
+                }
                 
                 // Deaccumulate precipitation
                 if isAccumulated {
@@ -210,11 +377,6 @@ struct DownloadEcmwfEcpdsCommand: AsyncCommand {
                 }
 
                 
-                // Scaling before compression with scalefactor
-                if let fma = variable.multiplyAdd(dtSeconds: dtSeconds) {
-                    grib2d.array.data.multiplyAdd(multiply: fma.multiply, add: fma.add)
-                }
-                
                 // Snow depth retrieved as water equivalent. Use snow density to calculate the actual snow depth.
                 if [EcmwfEcdpsIfsVariable.snow_density, .snow_depth].contains(variable) {
                     await inMemory.set(variable: variable, timestamp: timestamp, member: member, data: grib2d.array)
@@ -224,49 +386,16 @@ struct DownloadEcmwfEcpdsCommand: AsyncCommand {
                     }
                 }
 
-                /*if variable == .temperature_2m {
-                    await inMemory.set(variable: variable, timestamp: timestamp, member: member, data: grib2d.array)
-                }
-                
-                if variable == .dew_point_2m {
-                    await inMemory.set(variable: variable, timestamp: timestamp, member: member, data: grib2d.array)
-                    return
-                }
-                // Keep precip in memory for probability
-                if domain.countEnsembleMember > 1 && variable == .precipitation {
-                    await inMemory.set(variable: variable, timestamp: timestamp, member: member, data: grib2d.array)
-                }*/
-                
                 logger.info("Processing \(variable) member=\(member) unit=\(unit) stepType=\(stepType) stepRange=\(stepRange) timestep=\(timestamp.format_YYYYMMddHH)")
                 try await writer.write(member: member, variable: variable, data: grib2d.array.data)
             }
 
-            // Calculate mid/low/high/total cloudocover
-            /*logger.info("Calculating derived variables")
-            for member in 0..<domain.countEnsembleMember {
-                /// calculate RH 2m from dewpoint. Only store RH on disk.
-                if let dewpoint = await inMemory.get(variable: .dew_point_2m, timestamp: timestamp, member: member)?.data,
-                   let temperature = await inMemory.get(variable: .temperature_2m, timestamp: timestamp, member: member)?.data {
-                    let rh = zip(temperature, dewpoint).map(Meteorology.relativeHumidity)
-                    try await writer.write(member: member, variable: EcmwfVariable.relative_humidity_2m, data: rh)
-                }
-            }*/
-
-            /*if let writerProbabilities {
-                logger.info("Calculating precipitation probability")
-                try await inMemory.calculatePrecipitationProbability(
-                    precipitationVariable: .precipitation,
-                    dtHoursOfCurrentStep: hour - previousHour,
-                    writer: writerProbabilities
-                )
-            }*/
             
             let completed = i == timestamps.count - 1            
-            let handles = try await writer.finalise() + (writerProbabilities?.finalise() ?? [])
+            let handles = try await writer.finalise()
             try await uploadTask?.value
             uploadTask = Task {
                 try await writer.writeMetaAndAWSUpload(completed: completed, validTimes: Array(timestamps[0...i]), uploadS3Bucket: uploadS3Bucket)
-                try await writerProbabilities?.writeMetaAndAWSUpload(completed: completed, validTimes: Array(timestamps[0...i]), uploadS3Bucket: uploadS3Bucket)
             }
             return handles
         }
