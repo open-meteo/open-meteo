e669b5056d0f7eebf43d0e0bf27aedb349374be4
terraputix <terraputix@mailbox.org>
2026-05-15 22:42:31 +0200

    hashable version for groupedPreservedOrder
    For very large Sequences groupedPreservedOrder is slow due to O(N^2)
    behaviour.

--- Sources/App/Helper/Intrinsics/Sequence.swift
+++ Sources/App/Helper/Intrinsics/Sequence.swift
@@ -19,4 +19,29 @@ extension Sequence {
         }
         return groups
     }
+
+    /// Group the sequence by the given criterion.
+    ///
+    /// This Hashable version avoids O(N^2) runtime for large sequences!
+    /// - Parameter criterion: The basis by which to group the sequence.
+    /// - Throws: `Error`.
+    /// - Returns: Array of `(key: Group, values: [Element])`
+    func groupedPreservedOrder<Group: Hashable>(
+        by criterion: (Element) throws -> Group
+    ) rethrows -> [(key: Group, values: [Element])] {
+        var groupIndex: [Group: Int] = [:]
+        var groups: [(key: Group, values: [Element])] = []
+
+        for element in self {
+            let key = try criterion(element)
+            if let existingIndex = groupIndex[key] {
+                groups[existingIndex].values.append(element)
+            } else {
+                groupIndex[key] = groups.count
+                groups.append((key: key, values: [element]))
+            }
+        }
+
+        return groups
+    }
 }
