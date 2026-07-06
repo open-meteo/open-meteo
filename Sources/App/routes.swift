59c10bb88efeb8fda024aa7a14804b8be287df8c
terraputix <terraputix@mailbox.org>
2026-07-06 13:53:23 +0200

    route cancellation won't work like this

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -14,24 +14,7 @@ extension RoutesBuilder {
         _ path: PathComponent...,
         use closure: @Sendable @escaping (Request) async throws -> Response
     ) {
-        self.on(.GET, path, use: cancellableHandler(closure))
-        self.on(.POST, path, body: .collect(maxSize: "128kb"), use: cancellableHandler(closure))
-    }
-}
-
-private func cancellableHandler<Response: AsyncResponseEncodable & Sendable>(
-    _ closure: @Sendable @escaping (Request) async throws -> Response
-) -> @Sendable (Request) async throws -> Response {
-    return { req in
-        let task = Task {
-            try await closure(req)
-        }
-        req.body.drain { result in
-            if case .error = result {
-                task.cancel()
-            }
-            return req.eventLoop.makeSucceededVoidFuture()
-        }
-        return try await task.value
+        self.on(.GET, path, use: closure)
+        self.on(.POST, path, body: .collect(maxSize: "128kb"), use: closure)
     }
 }
