4b2c44529bf5233de50bb28636d5d47ef5ab531d
patrick-zippenfenig <patrick@zippenfenig.de>
2026-08-31 21:49:12 +0200

    log size difference at block preload

--- Sources/App/Helper/File/OmFileSystem+OmFiles.swift
+++ Sources/App/Helper/File/OmFileSystem+OmFiles.swift
@@ -43,9 +43,10 @@ extension OmFileLocalRemoteOmReader: OmFilePayload {
         let logger = reader.fn.backend.server.logger
         let activeBlocks = reader.fn.listOfActiveBlocks(maxAgeSeconds: 15*60)
         
+        /// TODO: Consider that the new file might be significantly bigger and the block placement is not correct anymore. It could be possible to map the actual used array data ranges from old block addresses to new ones.
         try await file.preloadBlocks(blocks: activeBlocks)
         let deletedBlocks = reader.fn.deleteCachedBlocks(olderThanSeconds: 60)
-        logger.warning("OmFileRemoteOmReader: Blocks freed=\(deletedBlocks) preloaded=\(activeBlocks.count). Updated file \(file.backend.object). ")
+        logger.warning("OmFileRemoteOmReader: Blocks freed=\(deletedBlocks) preloaded=\(activeBlocks.count). Updated file \(file.backend.object) (old size=\(reader.fn.count) new size=\(file.count)). ")
         return try await OmFileLocalRemoteOmReader(remoteFile: file)
     }
     
