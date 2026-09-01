ef238b1653223843671d811b81887ef22a6ef013
patrick-zippenfenig <patrick@zippenfenig.de>
2026-09-01 11:55:04 +0200

    bump om-file-format

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
         .package(url: "https://github.com/open-meteo/sdk.git", from: "1.28.0"),
-        .package(url: "https://github.com/open-meteo/om-file-format.git", revision: "e6a735533902d43a3704c0af2f8dd632110083f1"), // Because unsafe C flags are set, tagged releases cannot be used
+        .package(url: "https://github.com/open-meteo/om-file-format.git", revision: "f9dea31eba3280b4de39d9ce81acd559ce5bcc56"), // Because unsafe C flags are set, tagged releases cannot be used
         // .package(path: "../openmeteo-sdk-fork"),  // local forked version
         //.package(url: "https://github.com/open-meteo/sdk.git", branch: "add_ecmwf_long_window"),
         .package(url: "https://github.com/patrick-zippenfenig/curl-swift.git", from: "1.0.1"),
