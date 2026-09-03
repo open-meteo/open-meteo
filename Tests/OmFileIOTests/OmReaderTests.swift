9debeaeeb880308078ce8ba1ad3e77e16ae03c2c
Patrick Zippenfenig <patrick@zippenfenig.de>
2026-09-03 11:59:55 +0200

    fix: use correct memory barriers in atomic cache (#2095)

--- Tests/OmFileIOTests/OmReaderTests.swift
+++ Tests/OmFileIOTests/OmReaderTests.swift
@@ -117,11 +117,15 @@ import OmFileFormat
         let cache = try AtomicBlockCache(file: file, blockSize: 65536, blockCount: 50)
         let cacheFn = OmReaderBlockCache(backend: readFn, cache: AtomicCacheCoordinator(cache: cache), cacheKey: readFn.cacheKey)
         let read = try await OmFileReader(fn: cacheFn).expectArray(of: Float.self, io_size_max: 4096)
+        #expect(cacheFn.listOfActiveBlocks(maxAgeSeconds: 600) == [8..<9])
+        
         let value = try await read.readConcurrent(range: [0..<257, 511..<513])
         #expect(value[123] == 1218)
+        #expect(cacheFn.listOfActiveBlocks(maxAgeSeconds: 600) == [0..<4, 8..<9])
 
         let value2 = try await read.readConcurrent(range: [0..<257, 511..<513])
         #expect(value2[123] == 1218)
+        #expect(cacheFn.listOfActiveBlocks(maxAgeSeconds: 600) == [0..<4, 8..<9])
     }
 
     /*func testRemoteFileManager() async throws {
